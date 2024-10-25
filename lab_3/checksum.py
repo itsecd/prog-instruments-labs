284245e40daf0fd2b7c4476a1e825c15d1a29868
kksseonn <kote13241@gmail.com>
2024-10-25 13:24:43 +0400

    added serialize_result

--- lab_3/checksum.py
+++ lab_3/checksum.py
@@ -5,11 +5,6 @@ import csv
 import chardet
 from typing import List
 
-"""
-В этом модуле обитают функции, необходимые для автоматизированной проверки результатов ваших трудов.
-"""
-
-
 patterns = {
     'telephone': r'^"?\+7-\(\d{3}\)-\d{3}-\d{2}-\d{2}"?$',
     'height': r'^"?[1-2](?:\.\d{2})?"?$',
@@ -50,41 +45,26 @@ def process_csv(file_path):
     return invalid_rows
 
 def calculate_checksum(row_numbers: List[int]) -> str:
-    """
-    Вычисляет md5 хеш от списка целочисленных значений.
-
-    ВНИМАНИЕ, ВАЖНО! Чтобы сумма получилась корректной, считать, что первая строка с данными csv-файла имеет номер 0
-    Другими словами: В исходном csv 1я строка - заголовки столбцов, 2я и остальные - данные.
-    Соответственно, считаем что у 2 строки файла номер 0, у 3й - номер 1 и так далее.
-
-    Надеюсь, я расписал это максимально подробно.
-    Хотя что-то мне подсказывает, что обязательно найдется человек, у которого с этим возникнут проблемы.
-    Которому я отвечу, что все написано в докстринге ¯\_(ツ)_/¯
-
-    :param row_numbers: список целочисленных номеров строк csv-файла, на которых были найдены ошибки валидации
-    :return: md5 хеш для проверки через github action
-    """
     row_numbers.sort()
     return hashlib.md5(json.dumps(row_numbers).encode('utf-8')).hexdigest()
 
 
 def serialize_result(variant: int, checksum: str) -> None:
-    """
-    Метод для сериализации результатов лабораторной пишите сами.
-    Вам нужно заполнить данными - номером варианта и контрольной суммой - файл, лежащий в папке с лабораторной.
-    Файл называется, очевидно, result.json.
+    try:
+        with open('lab_3/result.json', 'r', encoding='utf-8') as json_file:
+            result_data = json.load(json_file)
+    except FileNotFoundError:
+        result_data = {"variant": variant, "checksum": checksum}
 
-    ВНИМАНИЕ, ВАЖНО! На json натравлен github action, который проверяет корректность выполнения лабораторной.
-    Так что не перемещайте, не переименовывайте и не изменяйте его структуру, если планируете успешно сдать лабу.
-
-    :param variant: номер вашего варианта
-    :param checksum: контрольная сумма, вычисленная через calculate_checksum()
-    """
-    pass
+    result_data['checksum'] = checksum
+    
+    with open('lab_3/result.json', 'w', encoding='utf-8') as json_file:
+        json.dump(result_data, json_file, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
     csv_file_path = 'lab_3/52.csv'
     invalid_row_numbers = process_csv(csv_file_path)
-    # print(calculate_checksum([1, 2, 3]))
-    # print(calculate_checksum([3, 2, 1]))
+    checksum = calculate_checksum(invalid_row_numbers)
+    variant_number = 52
+    serialize_result(variant_number, checksum)
