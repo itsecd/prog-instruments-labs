a75d4c7289ec019d5effe5091812735a571f04e5
kksseonn <kote13241@gmail.com>
2024-10-25 13:05:28 +0400

    fixed encoding error

--- lab_3/checksum.py
+++ lab_3/checksum.py
@@ -2,6 +2,7 @@ import json
 import hashlib
 import re
 import csv
+import chardet
 from typing import List
 
 """
@@ -31,51 +32,59 @@ def validate_row(row, row_number):
     return False
 
 def process_csv(file_path):
+    with open(file_path, 'rb') as f:
+        raw_data = f.read()
+        result = chardet.detect(raw_data)
+        encoding = result['encoding']
     invalid_rows = []
-    with open(file_path, newline='', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
-        next(reader)
-        for row_number, row in enumerate(reader):
-            if validate_row(row, row_number):
-                invalid_rows.append(row_number)
+    try:
+        with open(file_path, newline='', encoding=encoding) as csvfile:
+            reader = csv.reader(csvfile, delimiter=';')
+            next(reader)
+            for row_number, row in enumerate(reader):
+                if validate_row(row, row_number):
+                    invalid_rows.append(row_number)
+    except Exception as e:
+        print(f"Ошибка при чтении файла: {e}")
+    
     return invalid_rows
 
-# def calculate_checksum(row_numbers: List[int]) -> str:
-#     """
-#     Вычисляет md5 хеш от списка целочисленных значений.
+def calculate_checksum(row_numbers: List[int]) -> str:
+    """
+    Вычисляет md5 хеш от списка целочисленных значений.
 
-#     ВНИМАНИЕ, ВАЖНО! Чтобы сумма получилась корректной, считать, что первая строка с данными csv-файла имеет номер 0
-#     Другими словами: В исходном csv 1я строка - заголовки столбцов, 2я и остальные - данные.
-#     Соответственно, считаем что у 2 строки файла номер 0, у 3й - номер 1 и так далее.
+    ВНИМАНИЕ, ВАЖНО! Чтобы сумма получилась корректной, считать, что первая строка с данными csv-файла имеет номер 0
+    Другими словами: В исходном csv 1я строка - заголовки столбцов, 2я и остальные - данные.
+    Соответственно, считаем что у 2 строки файла номер 0, у 3й - номер 1 и так далее.
 
-#     Надеюсь, я расписал это максимально подробно.
-#     Хотя что-то мне подсказывает, что обязательно найдется человек, у которого с этим возникнут проблемы.
-#     Которому я отвечу, что все написано в докстринге ¯\_(ツ)_/¯
+    Надеюсь, я расписал это максимально подробно.
+    Хотя что-то мне подсказывает, что обязательно найдется человек, у которого с этим возникнут проблемы.
+    Которому я отвечу, что все написано в докстринге ¯\_(ツ)_/¯
 
-#     :param row_numbers: список целочисленных номеров строк csv-файла, на которых были найдены ошибки валидации
-#     :return: md5 хеш для проверки через github action
-#     """
-#     row_numbers.sort()
-#     return hashlib.md5(json.dumps(row_numbers).encode('utf-8')).hexdigest()
+    :param row_numbers: список целочисленных номеров строк csv-файла, на которых были найдены ошибки валидации
+    :return: md5 хеш для проверки через github action
+    """
+    row_numbers.sort()
+    return hashlib.md5(json.dumps(row_numbers).encode('utf-8')).hexdigest()
 
 
-# def serialize_result(variant: int, checksum: str) -> None:
-#     """
-#     Метод для сериализации результатов лабораторной пишите сами.
-#     Вам нужно заполнить данными - номером варианта и контрольной суммой - файл, лежащий в папке с лабораторной.
-#     Файл называется, очевидно, result.json.
+def serialize_result(variant: int, checksum: str) -> None:
+    """
+    Метод для сериализации результатов лабораторной пишите сами.
+    Вам нужно заполнить данными - номером варианта и контрольной суммой - файл, лежащий в папке с лабораторной.
+    Файл называется, очевидно, result.json.
 
-#     ВНИМАНИЕ, ВАЖНО! На json натравлен github action, который проверяет корректность выполнения лабораторной.
-#     Так что не перемещайте, не переименовывайте и не изменяйте его структуру, если планируете успешно сдать лабу.
+    ВНИМАНИЕ, ВАЖНО! На json натравлен github action, который проверяет корректность выполнения лабораторной.
+    Так что не перемещайте, не переименовывайте и не изменяйте его структуру, если планируете успешно сдать лабу.
 
-#     :param variant: номер вашего варианта
-#     :param checksum: контрольная сумма, вычисленная через calculate_checksum()
-#     """
-#     pass
+    :param variant: номер вашего варианта
+    :param checksum: контрольная сумма, вычисленная через calculate_checksum()
+    """
+    pass
 
 
 if __name__ == "__main__":
-    csv_file_path = 'lab_3/test.csv'
+    csv_file_path = 'lab_3/52.csv'
     invalid_row_numbers = process_csv(csv_file_path)
     # print(calculate_checksum([1, 2, 3]))
     # print(calculate_checksum([3, 2, 1]))
