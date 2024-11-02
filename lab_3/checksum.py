5720d11d019a38d85b195b36db4936dd92e2299d
Quyntrd <daniil.terebilkin@gmail.com>
2024-11-02 23:13:46 +0400

    added proccess_csv function

--- lab_3/checksum.py
+++ lab_3/checksum.py
@@ -63,7 +63,18 @@ def process_csv(file_path: str) -> List[int]:
     :param file_path: Path to the CSV file.
     :return: List of line numbers with errors.
     """
-    
+    invalid_rows: List[int] = []
+    encoding = define_encoding(file_path)
+    try:
+        with open(file_path, newline='', encoding=encoding) as csvfile:
+            reader = csv.reader(csvfile, delimiter=';')
+            next(reader)
+            for row_number, row in enumerate(reader):
+                if check_row(row, row_number):
+                    invalid_rows.append(row_number)
+    except Exception as exc:
+        raise Exception(f"Error occured during processing csv file {file_path}: {exc}")
+    return invalid_rows
 
 
 def calculate_checksum(row_numbers: List[int]) -> str:
