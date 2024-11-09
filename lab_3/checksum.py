e836ce694ea186e4a6792691f2f83754ecc6817d
Sapdotten <semenova.a.p.04@gmail.com>
2024-11-09 18:12:12 +0400

    write serilization

--- lab_3/checksum.py
+++ lab_3/checksum.py
@@ -23,7 +23,7 @@ def calculate_checksum(row_numbers: List[int]) -> str:
     :return: md5 хеш для проверки через github action
     """
     row_numbers.sort()
-    return hashlib.md5(json.dumps(row_numbers).encode('utf-8')).hexdigest()
+    return hashlib.md5(json.dumps(row_numbers).encode("utf-8")).hexdigest()
 
 
 def serialize_result(variant: int, checksum: str) -> None:
@@ -38,7 +38,9 @@ def serialize_result(variant: int, checksum: str) -> None:
     :param variant: номер вашего варианта
     :param checksum: контрольная сумма, вычисленная через calculate_checksum()
     """
-    pass
+    data = {"variant": str(variant), "checksum": checksum}
+    with open("lab_3\\result.json", 'w') as f:
+        json.dump(data, f)
 
 
 if __name__ == "__main__":
