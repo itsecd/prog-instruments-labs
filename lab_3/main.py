f6af72fdbc3c47055776e5115d6d3cab7ea7e0f3
Sapdotten <semenova.a.p.04@gmail.com>
2024-11-09 19:00:04 +0400

    Edit blood refular expression. Mistake was in '-'

--- lab_3/main.py
+++ lab_3/main.py
@@ -8,15 +8,15 @@ import checksum
 
 class CheckForm:
     REGEXES = {
-        "telephone": r"[+]\d[-][(]\d{3}[)][-]\d{3}[-]\d{2}[-]\d{2}",
+        "telephone": r"[+]7[-]\(\d{3}\)-\d{3}-\d{2}-\d{2}",
         "http_status_message": r"\d{3}\s\b[a-zA-Z\s]+",
-        "inn": r"\d{12}",
+        "inn": r"\d{10}|\d{12}",
         "identifier": r"\d{2}-\d{2}/\d{2}",
         "ip_v4": r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",
-        "latitude": r"-{0,1}\d{1,3}\.\d*",
-        "blood_type": r"([AB]|[O]){1,2}[\+-]",
+        "latitude": r"-{0,1}\d{1,2}\.\d*",
+        "blood_type": r"(A|B|AB|O)[+−]{1}",
         "isbn": r"(\d{3}-){0,1}\d-\d{5}-\d{3}-\d",
-        "uuid": r"[a-z0-9]{8}-([a-z0-9]{4}-){3}[a-z0-9]{12}",
+        "uuid": r"[a-f\d]{8}-([a-f\d]{4}-){3}[a-f\d]{12}",
         "date": r"\d{4}-\d{2}-\d{2}",
     }
 
@@ -40,10 +40,17 @@ class CheckForm:
                     fail_rows.append(i)
                     break
         return fail_rows
+        
 
 
 if __name__ == "__main__":
-    data = pd.read_csv("lab_3\\22.csv", sep=";", encoding="utf-16")
+    FILE_PATH = "lab_3\\22.csv"
+    data = pd.read_csv(FILE_PATH, sep=";", encoding="utf-16")
     error_rows = CheckForm.get_error_rows(data)
     check_sum = checksum.calculate_checksum(error_rows)
+    if check_sum != "048c9d1978185a45af355149a26625f5":
+        print(check_sum)
+        print("Not okay!")
+    else:
+        print("Okay!")
     checksum.serialize_result(22, check_sum)
