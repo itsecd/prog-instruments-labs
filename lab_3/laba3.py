eef299ce0db4adce46a2e390cb0792424a448b17
456 <ryzalen@yandex.ru>
2024-11-09 11:56:47 +0400

    Глобально переписаны регулярные выражения, немного изменены названия переменных для удобства

--- lab_3/laba3.py
+++ lab_3/laba3.py
@@ -1,30 +1,26 @@
-import csv
 import re
+import csv
 
-from checksum import serialize_result, calculate_checksum
+from checksum import calculate_checksum, serialize_result
 
 PATTERNS = {
-    "email": "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
-    "height": "^[1-2]\\.\\d{2}$",
-    "snils": "^\\d{11}$",
-    "passport": "^\\d{2}\\s\\d{2}\\s\\d{6}$",
-    "occupation": "^[a-zA-Zа-яА-ЯёЁ\\s-]+$",
-    "longitude": "^-?((180(\\.0+)?|1[0-7]?\\d(\\.\\d+)?|\\d{1,2}(\\.\\d+)?)|0(\\.\\d+)?)$",
-    "hex_color": "^#([a-f0-9]{6}|[a-f0-9]{3})$",
-    "issn": "^\\d{4}-\\d{4}$",
-    "locale_code": "^([a-z]{2,3})(?:-([a-zA-Z]{2,4}))?(?:-([a-zA-Z0-9-]+))?$",
-    "time": "^([0-1][0-9]|2[0-3])\\:[0-5][\\d]\\:[0-5][\\d]\\.[\\d]{6}"
-}
-
+    "email": "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+    "http_status_message":"^\d{3} [A-Za-z\s]+$",
+    "inn": "^\d{12}$",
+    "passport": "^\d{2} \d{2} \d{6}$",
+    "ip_v4":"^(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)$",
+    "latitude": "^(-?[1-8]?\d(?:\.\d{1,})?|90(?:\.0{1,})?)$",
+    "hex_color": "^#[0-9a-fA-F]{6}$",
+    "isbn": "\\d+-\\d+-\\d+-\\d+(:?-\\d+)?$",
+    "uuid": "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$",
+    "time": "^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)\.(\d{1,6})$"}
 
 def check_data(row: list) -> bool:
-    '''Проверяет строку на корректность данных по заданным паттернам.
-
-    Аргументы:
-    row -- список значений для проверки на соответствие паттернам
-
-    Возвращает:
-    bool -- True, если все значения соответствуют паттернам, иначе False
+    '''Проверяет строку таблицы на валидность
+    Args:
+        row (list): Строка таблицы для проверки
+    Returns:
+        bool: Результат прохождения/непрохождения проверки
     '''
     for key, value in zip(PATTERNS.keys(), row):
         if not re.match(PATTERNS[key], value):
@@ -32,45 +28,32 @@ def check_data(row: list) -> bool:
     return True
 
 
-def read_csv(filename: str) -> list:
-    '''Считывает данные из CSV файла и возвращает их в виде списка.
+def find_invalid_data(data: list) -> None:
+    '''Поиск индексов невалидных данных
+    Args:
+        data (list): Массив строк с данными
 
-        Аргументы:
-        filename -- название файла, который необходимо прочитать
-
-        Возвращает:
-        list -- список строк, считанных из файла, без заголовка
-        '''
-    data = []
-    with open(filename, "r", newline="", encoding="utf-16") as file:
-        reader = csv.reader(file, delimiter=";")
-        for el in reader:
-            data.append(el)
-        data.pop(0)
-        return data
-
-
-def find_incorrect_data(data: list) -> list:
-    '''Находит индексы некорректных данных в списке.
-
-        Аргументы:
-        data -- список данных, в котором необходимо проверить корректность
-
-        Возвращает:
-        list -- список индексов элементов, которые не прошли проверку
-        '''
-    indexes = []
+    Returns: None
+    '''
+    list_index = []
     index = 0
-    for el in data:
-        if not check_data(el):
-            indexes.append(index)
+    for elem in data:
+        if not check_data(elem):
+            list_index.append(index)
         index += 1
-    return indexes
+    serialize_result(21, calculate_checksum(list_index))
+
+
+def read_csv(file_name: str) -> list:
+    '''Десериализация данных из csv файла'''
+    list_data = []
+    with open(file_name, "r", newline="", encoding="utf-16") as file:
+        reader = csv.reader(file, delimiter=";")
+        for elem in reader:
+            list_data.append(elem)
+    list_data.pop(0)
+    return list_data
 
 
 if __name__ == "__main__":
-    filename = "21.csv"
-    variant = 21
-    data = read_csv(filename)
-    indexes = find_incorrect_data(data)
-    serialize_result(variant, calculate_checksum(indexes))
+    find_invalid_data(read_csv("21.csv"))
