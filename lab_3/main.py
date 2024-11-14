42bfac8cbfee039f470eebaa90996368c4ce0028
Шестериков Борис <boris20033@gmail.com>
2024-11-14 12:33:18 +0400

    Add RegEx in PATTERNS

--- lab_3/main.py
+++ lab_3/main.py
@@ -5,6 +5,19 @@ import os
 import pandas as pd
 import checksum as check
 
+PATTERNS = {
+    "email" : r"^\w+(\.\w+)*@\w+(\.\w+)+$",
+    "height" : r"^[1-2]\.\d{2}$",
+    "snils" : r"^\d{11}$",
+    "passport" : r"^\d{2}\s\d{2}\s\d{6}$",
+    "occupation" : r"^[а-яА-Яa-zA-Z\s-]*$",
+    "longitude" : r"^-?(180|1[0-7]\d|\d{1,2})(\.\d+)?$",
+    "hex_color" : r"#\d{6}",
+    "issn" : r"\d{4}-\d{4}",
+    "locale_code" : r"^[a-z]{1,3}(-[a-z]+)?(-[a-z]{2})?$",
+    "time" : r"^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)\.(\d{1,6})$"
+}
+
 def check_row():
     pass
 
