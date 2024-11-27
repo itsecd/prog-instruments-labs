772217003e9088db3826385d18349b7018a12c67
Sapdotten <semenova.a.p.04@gmail.com>
2024-11-27 13:06:43 +0400

    add settings for logger

--- lab_4/main.py
+++ lab_4/main.py
@@ -7,16 +7,23 @@ import logging
 from dotenv import load_dotenv
 import handlers
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    filename="py_log.log",
+    filemode="w",
+)
+
+
 def register_routers(dp):
     dp.include_routers(handlers.router)
 
 
-    
 async def main() -> None:
     """
     Entry point
     """
+    logging.info("Загрузка переменных из файла в окружение")
     load_dotenv()
     session = AiohttpSession()
     bot = Bot(os.getenv("BOT_TOKEN"), session=session)
@@ -24,11 +31,12 @@ async def main() -> None:
     register_routers(dp)
     try:
         await bot.delete_webhook()
-        await  asyncio.create_task(dp.start_polling(bot))
-        
+        await asyncio.create_task(dp.start_polling(bot))
+
     except Exception as ex:
         print(ex)
 
 
 if __name__ == "__main__":
+    logging.info("Старт процесса")
     asyncio.run(main())
