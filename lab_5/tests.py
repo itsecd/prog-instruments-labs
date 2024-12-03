92b2c5b602b5757d7568635c6bd58e3352f802bd
Egor <derasoft004@gmail.com>
2024-12-03 19:08:32 +0400

    lab_5-commit3: 4 tests for player position

--- lab_5/tests.py
+++ lab_5/tests.py
@@ -1,10 +1,31 @@
 import pytest
-import pygame
 
 from player import Player
 
 
-def player_position_test():
+def test_player_position():
     player = Player()
     assert player.rect.x == 400
     assert player.rect.y == 300
+
+
+def test_player_move():
+    player = Player()
+    player.move(10, 0)
+    assert player.rect.x == 410
+
+
+def test_player_dont_move_through_border():
+    player = Player()
+    player.move(1000, 0)
+    assert player.rect.x == 750
+
+
+def test_player_dont_move_through_border_twice():
+    player = Player()
+    player.move(-5000, 0)
+    assert player.rect.x == 0
+
+    player.move(10000, 0)
+    assert player.rect.x == 750
+
