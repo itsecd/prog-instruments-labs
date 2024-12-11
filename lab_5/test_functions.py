4cee6a60967fb411bc0a182bb085a3263522617b
Алексей Русяев <RusyaevAA@outlook.com>
2024-12-12 01:02:26 +0400

    read the documentation in the right way and updated action

--- lab_5/test_functions.py
+++ lab_5/test_functions.py
@@ -4,44 +4,41 @@ from functions import (read_the_file, frequency_bitwise_test,
                        same_consecutive_bits_test, longest_sequence_of_units_in_a_block_test)
 
 
-@pytest.mark.parametrize("path, information", 
-                         ["sequences/cxx.txt", 
-                          "11001000001111111010100100100110101011101101101110100111111001000000000101000110110000001001011000111110001010110001111000101110"])
-def reading_ability(path: str, information: str):
-    with open(path, "w", encoding="utf-8") as file:
-        file.write(information)
-    text = read_the_file(path)
-    assert text == information
+def test_reading_ability():
+    with open("lab_5/sequences/cxx.txt", "w", encoding="utf-8") as file:
+        file.write("11001000001111111010100100100110101011101101101110100111111001000000000101000110110000001001011000111110001010110001111000101110")
+    text = read_the_file("lab_5/sequences/cxx.txt")
+    assert text == "11001000001111111010100100100110101011101101101110100111111001000000000101000110110000001001011000111110001010110001111000101110"
 
-def frequency_bitwise_counting_ability():
+def test_frequency_bitwise_counting_ability():
     frequency = 0.8596837951986662
     sequence = "11001000001111111010100100100110101011101101101110100111111001000000000101000110110000001001011000111110001010110001111000101110"
     assert frequency == frequency_bitwise_test(sequence)
 
-@pytest.mark.parametrize("path, value", ["sequences/java.txt", 0.7236736098317631])
-def frequency_bitwise_counting_ability_with_reading(path: str, value: int):
+@pytest.mark.parametrize("path, value", [("lab_5/sequences/java.txt", 0.7236736098317631)])
+def test_frequency_bitwise_counting_ability_with_reading(path, value):
     sequence = read_the_file(path)
     frequency = frequency_bitwise_test(sequence)
     assert frequency == value
 
-def same_consecutive_bits_ability():
+def test_same_consecutive_bits_ability():
     frequency = 0.5977098069466218
     sequence = "11001000001111111010100100100110101011101101101110100111111001000000000101000110110000001001011000111110001010110001111000101110"
     assert frequency == same_consecutive_bits_test(sequence)
 
-@pytest.mark.parametrize("path, value", ["sequences/java.txt", 0.2934250460371556])
-def same_consecutive_bits_ability_with_reading(path: str, value: int):
+@pytest.mark.parametrize("path, value", [("lab_5/sequences/java.txt", 0.2934250460371556)])
+def test_same_consecutive_bits_ability_with_reading(path, value):
     sequence = read_the_file(path)
     frequency = same_consecutive_bits_test(sequence)
     assert frequency == value
 
-def longest_sequence_of_units_in_a_block_checking():
+def test_longest_sequence_of_units_in_a_block_checking():
     frequency = 0.16070143844190707
     sequence = "11001000001111111010100100100110101011101101101110100111111001000000000101000110110000001001011000111110001010110001111000101110"
     assert frequency == longest_sequence_of_units_in_a_block_test(sequence)
 
-@pytest.mark.parametrize("path, value", ["sequences/java.txt", 0.854898667197296])
-def longest_sequence_of_units_in_a_block_checking_with_reading(path: str, value: int):
+@pytest.mark.parametrize("path, value", [("lab_5/sequences/java.txt", 0.854898667197296)])
+def test_longest_sequence_of_units_in_a_block_checking_with_reading(path, value):
     sequence = read_the_file(path)
     frequency = longest_sequence_of_units_in_a_block_test(sequence)
     assert frequency == value
