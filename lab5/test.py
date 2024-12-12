560c59bbe4c724a40b84f9d1812b2ab06612a3eb
456 <ryzalen@yandex.ru>
2024-12-12 20:23:04 +0400

    добавлен тест на поиск книги по автору

--- lab5/test.py
+++ lab5/test.py
@@ -12,3 +12,9 @@ def test_find_book_by_title(library_setup):
     results = library_setup.find_book(title="Гарри Поттер")
     assert len(results) == 1
     assert results[0].title == "Гарри Поттер и философский камень"
+
+
+def test_find_book_by_author(library_setup):
+    results = library_setup.find_book(author="Толстой")
+    assert len(results) == 1
+    assert results[0].author == "Лев Толстой"
