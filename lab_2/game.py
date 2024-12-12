ef1a0d995650e604256ef0264871dfbc78ef2663
Ilya Ovchinnikov <ovchin98ilya@gmail.com>
2024-12-12 13:13:40 +0400

    sources added

---
+++ lab_2/game.py
@@ -0,0 +1,8 @@
+def play_game(TennisGame, p1Points, p2Points, p1Name, p2Name):
+    game = TennisGame(p1Name, p2Name)
+    for i in range(max(p1Points, p2Points)):
+        if i < p1Points:
+            game.won_point(p1Name)
+        if i < p2Points:
+            game.won_point(p2Name)
+    return game
