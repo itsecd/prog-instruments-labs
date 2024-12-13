067c7bcc1b9e7e34c3f6ab8b2163cc3fd02e389d
ynnmuraii <tihookeanski.y.tanruuu@gmail.com>
2024-12-13 23:45:34 +0400

    add path.json, complete serialize func, maked 10 regex

--- lab_3/checksum.py
+++ lab_3/checksum.py
@@ -1,46 +1,95 @@
 import json
 import hashlib
-from typing import List
+import csv
+import re
+from typing import Dict, List
 
-"""
-В этом модуле обитают функции, необходимые для автоматизированной проверки результатов ваших трудов.
-"""
+patterns: Dict[str, str] = {
+    "email": r"^[a-z0-9]+(?:[._][a-z0-9]+)*\@[a-z]+(?:\.[a-z]+)+$",
+    "height": r"^[1-2]\.\d{2}$",
+    "inn": r"^\d{12}$",
+    "passport": r"^\d{2}\s\d{2}\s\d{6}$",
+    "occupation": r"[a-zA-Zа-яА-ЯёЁ -]+",
+    "latitude": r"^-?(90|[0-8]?[0-9])\.\d+$",
+    "hex_color": r"^\#[0-9a-fA-F]{6}$",
+    "issn": r"^\d{4}-\d{4}$",
+    "uuid": r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
+    "time": r"^(2[0-3]|[0-1][0-9]):[0-5][0-9]:[0-5][0-9]\.\d{6}$"
+}
+
+
+def validate_row(fields: List[str]) -> bool:
+    """
+    Validates each field of the string according to the specified regular expressions.
+
+    :param fields: A list of the values of the string fields.
+    :return: False if a mismatched record is found, otherwise True.
+    """
+    for idx, field in enumerate(fields):
+        pattern_key = list(patterns.keys())[idx]
+        regex = patterns.get(pattern_key)
+        if not re.fullmatch(regex, field):
+            return False
+    return True
+
+
+def process_csv(file_path: str) -> List[int]:
+    """
+    Processes a CSV file for error rows.
+
+    :param file_path: Path to the CSV file.
+    :return: List of row lines with errors.
+    """
+    invalid_rows: List[int] = []
+    with open(file_path, newline='', encoding='utf-16') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        next(reader)
+        
+        for row_number, row in enumerate(reader, start=1):
+            if not validate_row(list(row.values())):
+                invalid_rows.append(row_number)
+
+    return invalid_rows
 
 
 def calculate_checksum(row_numbers: List[int]) -> str:
     """
-    Вычисляет md5 хеш от списка целочисленных значений.
+    Calculates a checksum for a list of row numbers.
 
-    ВНИМАНИЕ, ВАЖНО! Чтобы сумма получилась корректной, считать, что первая строка с данными csv-файла имеет номер 0
-    Другими словами: В исходном csv 1я строка - заголовки столбцов, 2я и остальные - данные.
-    Соответственно, считаем что у 2 строки файла номер 0, у 3й - номер 1 и так далее.
-
-    Надеюсь, я расписал это максимально подробно.
-    Хотя что-то мне подсказывает, что обязательно найдется человек, у которого с этим возникнут проблемы.
-    Которому я отвечу, что все написано в докстринге ¯\_(ツ)_/¯
-
-    :param row_numbers: список целочисленных номеров строк csv-файла, на которых были найдены ошибки валидации
-    :return: md5 хеш для проверки через github action
+    :param row_numbers: List of row numbers.
+    :return: Checksum in MD5 format.
     """
     row_numbers.sort()
     return hashlib.md5(json.dumps(row_numbers).encode('utf-8')).hexdigest()
 
 
-def serialize_result(variant: int, checksum: str) -> None:
+def serialize_to_json(variant: int, checksum: str) -> None:
     """
-    Метод для сериализации результатов лабораторной пишите сами.
-    Вам нужно заполнить данными - номером варианта и контрольной суммой - файл, лежащий в папке с лабораторной.
-    Файл называется, очевидно, result.json.
+    Serializes the result to a JSON file.
 
-    ВНИМАНИЕ, ВАЖНО! На json натравлен github action, который проверяет корректность выполнения лабораторной.
-    Так что не перемещайте, не переименовывайте и не изменяйте его структуру, если планируете успешно сдать лабу.
-
-    :param variant: номер вашего варианта
-    :param checksum: контрольная сумма, вычисленная через calculate_checksum()
+    :param variant: Variant number.
+    :param checksum: Checksum.
     """
-    pass
+    with open("lab_3/path_csv.json", "r", encoding='utf-8') as options_file:
+        options = json.load(options_file)
+        result_file_path = options["result_file_path"]
+
+    with open(result_file_path, 'w', encoding='utf-8') as file:
+        result = {
+            "variant": variant,
+            "checksum": checksum
+        }
+        json.dump(result, file, ensure_ascii=False)
 
 
 if __name__ == "__main__":
-    print(calculate_checksum([1, 2, 3]))
-    print(calculate_checksum([3, 2, 1]))
+
+    with open("lab_3/path_csv.json", "r", encoding='utf-8') as options_file:
+        options = json.load(options_file)
+
+    invalid_data = process_csv(options["csv_file_path"])
+    print("Число невалидных строк:", len(invalid_data))
+
+    variant = 47
+    checksum = calculate_checksum(invalid_data)
+    serialize_to_json(variant, checksum)
