af9e29bcfa4a70cf73154fe8949df5a620775ecb
PadimVetrov <padimvetrov@gmail.com>
2025-09-09 17:09:28 +0400

    Исправил названия методов

--- lab_1/test_yahtzee/yahtzee.py
+++ lab_1/test_yahtzee/yahtzee.py
@@ -23,75 +23,74 @@ class Yahtzee:
     @staticmethod
     def ones(d1, d2, d3, d4, d5):
         sum = 0
-        if (d1 == 1):
+        if d1 == 1:
             sum += 1
-        if (d2 == 1):
+        if d2 == 1:
             sum += 1
-        if (d3 == 1):
+        if d3 == 1:
             sum += 1
-        if (d4 == 1):
+        if d4 == 1:
             sum += 1
-        if (d5 == 1):
+        if d5 == 1:
             sum += 1
         return sum
 
     @staticmethod
     def twos(d1, d2, d3, d4, d5):
         sum = 0
-        if (d1 == 2):
+        if d1 == 2:
             sum += 2
-        if (d2 == 2):
+        if d2 == 2:
             sum += 2
-        if (d3 == 2):
+        if d3 == 2:
             sum += 2
-        if (d4 == 2):
+        if d4 == 2:
             sum += 2
-        if (d5 == 2):
+        if d5 == 2:
             sum += 2
         return sum
 
     @staticmethod
     def threes(d1, d2, d3, d4, d5):
         s = 0
-        if (d1 == 3):
+        if d1 == 3:
             s += 3
-        if (d2 == 3):
+        if d2 == 3:
             s += 3
-        if (d3 == 3):
+        if d3 == 3:
             s += 3
-        if (d4 == 3):
+        if d4 == 3:
             s += 3
-        if (d5 == 3):
+        if d5 == 3:
             s += 3
         return s
 
-    def __init__(self, d1, d2, d3, d4, _5):
+    def __init__(self, d1, d2, d3, d4, d5):
         self.dice = [0] * 5
         self.dice[0] = d1
         self.dice[1] = d2
         self.dice[2] = d3
         self.dice[3] = d4
-        self.dice[4] = _5
+        self.dice[4] = d5
 
     def fours(self):
         sum = 0
         for at in range(5):
-            if (self.dice[at] == 4):
+            if self.dice[at] == 4:
                 sum += 4
         return sum
 
     def fives(self):
         s = 0
-        i = 0
         for i in range(len(self.dice)):
-            if (self.dice[i] == 5):
+            if self.dice[i] == 5:
                 s = s + 5
         return s
 
     def sixes(self):
         sum = 0
         for at in range(len(self.dice)):
-            if (self.dice[at] == 6):
+            if self.dice[at] == 6:
                 sum = sum + 6
         return sum
 
@@ -103,9 +102,8 @@ class Yahtzee:
         counts[d3 - 1] += 1
         counts[d4 - 1] += 1
         counts[d5 - 1] += 1
-        at = 0
         for at in range(6):
-            if (counts[6 - at - 1] == 2):
+            if counts[6 - at - 1] == 2:
                 return (6 - at) * 2
         return 0
 
@@ -120,24 +118,24 @@ class Yahtzee:
         n = 0
         score = 0
         for i in range(6):
-            if (counts[6 - i - 1] == 2):
+            if counts[6 - i - 1] == 2:
                 n = n + 1
                 score += (6 - i)
-        if (n == 2):
+        if n == 2:
             return score * 2
         else:
             return 0
 
     @staticmethod
-    def four_of_a_kind(_1, _2, d3, d4, d5):
+    def four_of_a_kind(d1, d2, d3, d4, d5):
         tallies = [0] * 6
-        tallies[_1 - 1] += 1
-        tallies[_2 - 1] += 1
+        tallies[d1 - 1] += 1
+        tallies[d2 - 1] += 1
         tallies[d3 - 1] += 1
         tallies[d4 - 1] += 1
         tallies[d5 - 1] += 1
         for i in range(6):
-            if (tallies[i] == 4):
+            if tallies[i] == 4:
                 return (i + 1) * 4
         return 0
 
@@ -150,12 +148,12 @@ class Yahtzee:
         t[d4 - 1] += 1
         t[d5 - 1] += 1
         for i in range(6):
-            if (t[i] == 3):
+            if t[i] == 3:
                 return (i + 1) * 3
         return 0
 
     @staticmethod
-    def smallStraight(d1, d2, d3, d4, d5):
+    def small_straight(d1, d2, d3, d4, d5):
         tallies = [0] * 6
         tallies[d1 - 1] += 1
         tallies[d2 - 1] += 1
@@ -171,7 +169,7 @@ class Yahtzee:
         return 0
 
     @staticmethod
-    def largeStraight(d1, d2, d3, d4, d5):
+    def large_straight(d1, d2, d3, d4, d5):
         tallies = [0] * 6
         tallies[d1 - 1] += 1
         tallies[d2 - 1] += 1
@@ -187,13 +185,12 @@ class Yahtzee:
         return 0
 
     @staticmethod
-    def fullHouse(d1, d2, d3, d4, d5):
+    def full_house(d1, d2, d3, d4, d5):
         tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
+        has_two = False
+        two_at = 0
+        has_three = False
+        three_at = 0
 
         tallies = [0] * 6
         tallies[d1 - 1] += 1
@@ -203,16 +200,16 @@ class Yahtzee:
         tallies[d5 - 1] += 1
 
         for i in range(6):
-            if (tallies[i] == 2):
-                _2 = True
-                _2_at = i + 1
+            if tallies[i] == 2:
+                has_two = True
+                two_at = i + 1
 
         for i in range(6):
-            if (tallies[i] == 3):
-                _3 = True
-                _3_at = i + 1
+            if tallies[i] == 3:
+                has_three = True
+                three_at = i + 1
 
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
+        if has_two and has_three:
+            return two_at * 2 + three_at * 3
         else:
             return 0
