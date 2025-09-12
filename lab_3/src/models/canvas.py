e2f44201ffab2d1105df9431b6af2026ae567c31
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-13 03:04:51 +0400

    Add first canvas class

---
+++ lab_3/src/models/canvas.py
@@ -0,0 +1,22 @@
+from .construction import Construction
+
+
+class Canvas(Construction):
+    def __init__(self, width: int, height: int, fill: str):
+        if width <= 0:
+            raise ValueError("width cannot be <= 0")
+
+        if height <= 0:
+            raise ValueError("height cannot be <= 0")
+
+        self.fill = fill
+        self.width = width
+        self.height = height
+
+    def __repr__(self):
+        return f"Canvas({self.width}, {self.height}, '{self.fill}')"
+
+    def construct(self, where: list[list[str]]):
+        for row in range(self.height):
+            for col in range(self.width):
+                where[row][col] = self.fill
