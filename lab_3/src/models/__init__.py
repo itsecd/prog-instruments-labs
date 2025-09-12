56852dda377bca183201889acf1399beb54cf518
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-13 03:01:50 +0400

    Add first rect class

--- lab_3/src/models/__init__.py
+++ lab_3/src/models/__init__.py
@@ -1,3 +1,4 @@
 from .construction import Construction
 from .circle import Circle
 from .line import Line
+from .rect import Rect
---
+++ lab_3/src/models/rect.py
@@ -0,0 +1,17 @@
+from .construction import Construction
+
+
+class Rect(Construction):
+    def __init__(
+        self, x1: int, y1: int, x2: int, y2: int,
+        border: str, fill: str | None = None,
+    ):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.border = border
+        self.fill = fill
+
+    def __repr__(self):
+        return f"Rect({self.x1}, {self.y1}, {self.x2}, {self.y2}, '{self.border}', '{self.fill}')"
