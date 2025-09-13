547c2db0a10d65dd7ff6cf2da8bcf4d096b093bc
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-13 15:16:10 +0400

    Replace 2d list to surface class

--- lab_3/src/askill/models/construction.py
+++ lab_3/src/askill/models/construction.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
+from .surface import Surface
 
 
 class Construction:#(ABC):
     # @abstractmethod
-    def construct(self, where: list[list[str]]): pass
+    def construct(self, surface: Surface): pass
