1cfdc9680ea54870459366aa89aa1de1b3a2be99
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-13 15:10:05 +0400

    Add string parse for rect and canvas

--- lab_3/src/askill/models/__init__.py
+++ lab_3/src/askill/models/__init__.py
@@ -2,4 +2,5 @@ from .construction import Construction
 from .circle import Circle
 from .line import Line
 from .rect import Rect
+from .canvas import Canvas
 from .surface import Surface
--- lab_3/src/askill/parser/parser.py
+++ lab_3/src/askill/parser/parser.py
@@ -1,16 +1,37 @@
-from ..models.construction import Construction
+import re
+from ..models import Construction, Rect, Canvas
 
 
 class Parser:
     _PATTERNS = {
-        "canvas": r"^\# (?P<cmd>CANVAS) +\"(?P<fill>.)\" +\((?P<width>\d+),(?P<height>\d+)\)\s*\#*$",
-        "rect": r"^(?P<cmd>RECT) +\"(?P<border>.)\" +\((?P<x1>\d+),(?P<y1>\d+)\) -> \((?P<x2>\d+),(?P<y2>\d+)\)( FILL \"(?P<fill>.)\")?\s*\#*$",
+        "canvas": r"^\# (?P<command>CANVAS) +\"(?P<fill>.)\" +\((?P<width>\d+),(?P<height>\d+)\)\s*\#*$",
+        "rect": r"^(?P<command>RECT) +\"(?P<border>.)\" +\((?P<x1>\d+),(?P<y1>\d+)\) -> \((?P<x2>\d+),(?P<y2>\d+)\)( FILL \"(?P<fill>.)\")?\s*\#*$",
     }
 
     @classmethod
     def _parse_string(cls, string: str) -> Construction:
-        pass
+        for _, pattern in cls._PATTERNS.items():
+            matches = re.match(pattern, string)
+            if matches is None:
+                continue
+
+            group_dict = matches.groupdict()
+            command = group_dict.pop("command")
+
+            if command == "RECT":
+                return Rect(
+                    int(group_dict["x1"]), int(group_dict["y1"]),
+                    int(group_dict["x2"]), int(group_dict["y2"]),
+                    group_dict["border"], group_dict["fill"],
+                )
+            elif command == "CANVAS":
+                return Canvas(
+                    int(group_dict["width"]), int(group_dict["height"]),
+                    group_dict["fill"],
+                )
+        raise RuntimeError(f"Bad parsing: {string}")
 
     @classmethod
     def parse(cls, text: str) -> list[Construction]:
         pass
+
