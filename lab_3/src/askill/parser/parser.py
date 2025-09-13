f18801dbb9e3df693bd5ed6620c0b1ba11a87ab3
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-13 10:02:48 +0400

    Add parser class

---
+++ lab_3/src/askill/parser/parser.py
@@ -0,0 +1,11 @@
+from ..models.construction import Construction
+
+
+class Parser:
+    @classmethod
+    def _parse_string(cls, string: str) -> Construction:
+        pass
+
+    @classmethod
+    def parse(cls, text: str) -> list[Construction]:
+        pass
