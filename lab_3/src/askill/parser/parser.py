329ac1e7082e527df65549faac39bae975b7878c
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-13 10:05:52 +0400

    Add patterns for canvas and rect to parser

--- lab_3/src/askill/parser/parser.py
+++ lab_3/src/askill/parser/parser.py
@@ -2,6 +2,11 @@ from ..models.construction import Construction
 
 
 class Parser:
+    _PATTERNS = {
+        "canvas": r"^\# (?P<cmd>CANVAS) +\"(?P<fill>.)\" +\((?P<width>\d+),(?P<height>\d+)\)\s*\#*$",
+        "rect": r"^(?P<cmd>RECT) +\"(?P<border>.)\" +\((?P<x1>\d+),(?P<y1>\d+)\) -> \((?P<x2>\d+),(?P<y2>\d+)\)( FILL \"(?P<fill>.)\")?\s*\#*$",
+    }
+
     @classmethod
     def _parse_string(cls, string: str) -> Construction:
         pass
