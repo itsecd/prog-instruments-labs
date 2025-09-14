9ac085e780c937a823abfc3d89014fe755208109
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-14 19:07:39 +0400

    Add raw 2d coords regex

--- lab_3/src/askill/parser/patterns.py
+++ lab_3/src/askill/parser/patterns.py
@@ -7,6 +7,8 @@ _END = fr"\s*{_COMMENT}*$"  # "... #..."
 _RAW_NUM = r"(?P<{}>\d+)"  # "123"
 _RAW_SYMB = r"\"(?P<{}>.)\""  # '"s"'
 
+_RAW_COORDS_2D = fr"\({_RAW_NUM},\s*{_RAW_NUM}\)"  # "(1, 2)"
+
 
 
 if __name__ == "__main__":
