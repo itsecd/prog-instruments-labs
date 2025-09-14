2752e3cf6bf80fb266f9e240170b0785a0a7ac78
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-14 19:27:20 +0400

    Change string start regex, add docstrings for rect and canvas

--- lab_3/src/askill/parser/patterns.py
+++ lab_3/src/askill/parser/patterns.py
@@ -1,7 +1,7 @@
 import re
 
 COMMENT = r"\#"  # "#"
-START = r"^"
+START = r"^\s*"
 END = fr"\s*{COMMENT}?.*$"  # "... #..."
 
 RAW_NUM = r"(?P<{}>\d+)"  # "123"
@@ -13,16 +13,28 @@ RAW_FROM_TO_2D = fr"{RAW_COORDS_2D}\s+->\s+{RAW_COORDS_2D}"  # "(1,2)  -> (3, 4)
 RAW_FILL_ARG = fr"FILL\s+{RAW_SYMB}"  # 'FILL  "X"'
 
 CANVAS_CMD = fr"{START}{COMMENT}\s*CANVAS\s+{RAW_SYMB.format('fill')}\s+{RAW_COORDS_2D.format('width', 'height')}{END}"
-# '# CANVAS "x" (24, 24)'
+"""Examples:
+```
+'# CANVAS "x" (24, 24)'
+```
+"""
 
 RECT_CMD = fr"{START}\s*RECT\s+{RAW_SYMB.format('border')}\s+{RAW_FROM_TO_2D.format('x1', 'y1', 'x2', 'y2')}( {RAW_FILL_ARG.format('fill')})?{END}"
+"""Examples:
+```
+'RECT "X" (1,2) -> (3, 4) FILL "X" # comment'
+'RECT "Y" (0,0) -> (1, 1)'
+```
+"""
+
+CIRCLE_CMD = fr"{START}\s"
 
 print(RECT_CMD)
 
 if __name__ == "__main__":
     strings = {
         "canvas": '# CANVAS "x" (24, 24)',
-        "rect": 'RECT "X" (1,2) -> (3, 4)',
+        "rect": 'RECT "X" (1,2) -> (3, 4) FILL "X"',
     }
 
     matches = {
