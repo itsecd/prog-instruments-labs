82be03000cabce0c1c4177b8ef9eb32029b96d62
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-14 19:08:18 +0400

    Add 2d from-to regex

--- lab_3/src/askill/parser/patterns.py
+++ lab_3/src/askill/parser/patterns.py
@@ -9,6 +9,7 @@ _RAW_SYMB = r"\"(?P<{}>.)\""  # '"s"'
 
 _RAW_COORDS_2D = fr"\({_RAW_NUM},\s*{_RAW_NUM}\)"  # "(1, 2)"
 _RAW_COORDS_3D = fr"\({_RAW_NUM},\s*{_RAW_NUM},\s*{_RAW_NUM}\)"  # "(1, 2, 3)"
+_RAW_FROM_TO_2D = fr"{_RAW_COORDS_2D}\s+->\s+{_RAW_COORDS_2D}"  # "(1,2)  -> (3, 4)"
 
 
 if __name__ == "__main__":
