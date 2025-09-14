1e879531c5ad05913887f9a850e00286b4c91cbd
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-14 19:06:05 +0400

    Add comment regex

--- lab_3/src/askill/parser/patterns.py
+++ lab_3/src/askill/parser/patterns.py
@@ -1,6 +1,8 @@
-_PATTERNS = {
-    "canvas": r"^\# CANVAS +\"(?P<fill>.)\" +\((?P<width>\d+),(?P<height>\d+)\)\s*\#*$",
-    "rect": r"^RECT +\"(?P<border>.)\" +\((?P<x1>\d+),(?P<y1>\d+)\) -> \((?P<x2>\d+),(?P<y2>\d+)\)( FILL \"(?P<fill>.)\")?\s*\#*$",
-    "circle": r"^CIRCLE +\"(?P<border>.)\" +\((?P<x>\d+),(?P<y>\d+),(?P<r>\d+)\)( FILL \"(?P<fill>.)\")?\s*$",
-    "line": r"^LINE +\"(?P<fill>.)\" +\((?P<x1>\d+),(?P<y1>\d+)\) -> \((?P<x2>\d+),(?P<y2>\d+)\)\s*$",
-}
+import re
+
+_COMMENT = r"\#"  # "#"
+
+
+
+if __name__ == "__main__":
+    pass
