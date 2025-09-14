58ea9056aee2a0a4e5ff2df53ed95be97447e9e2
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-14 19:07:56 +0400

    Add raw 3d coords regex

--- lab_3/src/askill/parser/patterns.py
+++ lab_3/src/askill/parser/patterns.py
@@ -8,7 +8,7 @@ _RAW_NUM = r"(?P<{}>\d+)"  # "123"
 _RAW_SYMB = r"\"(?P<{}>.)\""  # '"s"'
 
 _RAW_COORDS_2D = fr"\({_RAW_NUM},\s*{_RAW_NUM}\)"  # "(1, 2)"
-
+_RAW_COORDS_3D = fr"\({_RAW_NUM},\s*{_RAW_NUM},\s*{_RAW_NUM}\)"  # "(1, 2, 3)"
 
 
 if __name__ == "__main__":
