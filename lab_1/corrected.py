0e57eac5e695ffd19cf18345c36f148d11ecece3
Anzor Magomedagaev <anzor.m13@gmail.com>
2025-09-16 02:25:36 +0400

    удалены избыточные комментарии и улучшены документацию методов

--- lab_1/corrected.py
+++ lab_1/corrected.py
@@ -1,17 +1,8 @@
 # This is like making a package.lock file for npm package.
 # Yes, I should be making it.
 __author__ = "Nitkarsh Chourasia"
-__version__ = "0.0.0"  # SemVer # Understand more about it
-__license__ = "MIT"  # Understand more about it
-# Want to make it open source but how to do it?
-# Program to make a simple calculator
-# Will have to extensively work on Jarvis and local_document
-# and MongoDb and Redis and JavaScript and CSS and DOM manipulation
-# to understand it.
-# Will have to study maths to understand it more better.
-# How can I market gtts? Like showing used google's api?
-# This is how can I market it?
-# Project description? What will be the project description?
+__version__ = "0.0.0"  # SemVer
+__license__ = "MIT"
 
 from gtts import gTTS
 from pygame import mixer, time
@@ -19,18 +10,6 @@ from io import BytesIO
 from pprint import pprint
 
 
-# Find the best of best extensions for the auto generation of
-# the documentation parts.
-# For your favourite languages like JavaScript, Python ,etc,...
-# Should be able to print date and time too.
-# Should use voice assistant for specially abled people.
-# A fully personalised calculator.
-# voice_assistant on/off , setting bool value to true or false
-
-# Is the operations valid?
-
-
-# Validation checker
 class Calculator:
     def __init__(self):
         self.take_inputs()
@@ -65,7 +44,6 @@ class Calculator:
         Returns:
             float: The quotient of num1 divided by num2.
         """
-        # What do we mean by quotient?
         return self.num1 / self.num2
 
     def power(self):
@@ -74,7 +52,7 @@ class Calculator:
         Returns:
             float: num1 raised to the power of num2.
         """
-        return self.num1 ** self.num2  # ИСПРАВЛЕНО: добавлены пробелы вокруг **
+        return self.num1 ** self.num2
 
     def root(self):
         """Get the root of numbers.
@@ -82,7 +60,7 @@ class Calculator:
         Returns:
             float: num2-th root of num1.
         """
-        return self.num1 ** (1 / self.num2)  # ИСПРАВЛЕНО: добавлены пробелы
+        return self.num1 ** (1 / self.num2)
 
     def remainer(self):
         """Get the remainder of numbers.
@@ -90,8 +68,7 @@ class Calculator:
         Returns:
             float: The remainder of num1 divided by num2.
         """
-        # Do I have to use the '.' period or full_stop in the numbers?
-        return self.num1 % self.num2  # ИСПРАВЛЕНО: добавлены пробелы вокруг %
+        return self.num1 % self.num2
 
     def cube_root(self):
         """Get the cube root of numbers.
@@ -99,7 +76,7 @@ class Calculator:
         Returns:
             float: The cube root of num1.
         """
-        return self.num1 ** (1 / 3)  # ИСПРАВЛЕНО: добавлены пробелы
+        return self.num1 ** (1 / 3)
 
     def cube_exponent(self):
         """Get the cube exponent of numbers.
@@ -107,7 +84,7 @@ class Calculator:
         Returns:
             float: num1 cubed.
         """
-        return self.num1 ** 3  # ИСПРАВЛЕНО: добавлены пробелы вокруг **
+        return self.num1 ** 3
 
     def square_root(self):
         """Get the square root of numbers.
@@ -115,7 +92,7 @@ class Calculator:
         Returns:
             float: The square root of num1.
         """
-        return self.num1 ** (1 / 2)  # ИСПРАВЛЕНО: добавлены пробелы
+        return self.num1 ** (1 / 2)
 
     def square_exponent(self):
         """Get the square exponent of numbers.
@@ -123,7 +100,7 @@ class Calculator:
         Returns:
             float: num1 squared.
         """
-        return self.num1 ** 2  # ИСПРАВЛЕНО: добавлены пробелы вокруг **
+        return self.num1 ** 2
 
     def factorial(self):
         """Get the factorial of numbers."""
@@ -133,27 +110,23 @@ class Calculator:
         """Get the list of factors of numbers."""
         pass
 
-    def calculate_factorial(self):  # ИСПРАВЛЕНО: переименован метод
+    def calculate_factorial(self):
         """Calculate factorial of a number."""
         result = 1
         for i in range(1, self.num + 1):
-            result = result * i  # ИСПРАВЛЕНО: добавлены пробелы вокруг *
+            result = result * i
         return result
 
-    def lcm(self):  # ИСПРАВЛЕНО: переименован в нижний регистр
+    def lcm(self):
         """Get the LCM of numbers."""
         pass
 
-    def hcf(self):  # ИСПРАВЛЕНО: переименован в нижний регистр
+    def hcf(self):
         """Get the HCF of numbers."""
         pass
 
-    # class time: # Working with days calculator
     def age_calculator(self):
         """Get the age of the user."""
-        # This is be very accurate and precise it should include
-        # proper leap year and last birthday till now every detail.
-        # Should show the preciseness in seconds when called.
         pass
 
     def days_calculator(self):
@@ -170,7 +143,7 @@ class Calculator:
 
     class Trigonometry:
         """Class enriched with all the methods to solve
-        basic trignometric problems
+        basic trigonometric problems.
         """
 
         def pythagorean_theorem(self):
@@ -189,74 +162,53 @@ class Calculator:
             """Get the perpendicular of the user."""
             pass
 
-    # class Logarithms:
-    # Learn more about Maths in general
-
     def quadratic_equation(self):
         """Get the quadratic equation of the user."""
         pass
 
     def open_system_calculator(self):
-        """Open the calculator present on the machine
-        of the user
-        """
-        # first identify the os
-        # track the calculator
-        # add a debugging feature like error handling
-        # for linux and mac
-        # if no such found then print a message to the user
-        # that sorry dear it wasn't possible to so
-        # then open it
+        """Open the calculator present on the machine of the user."""
+        pass
 
     def take_inputs(self):
-        """Take the inputs from the user in proper sucession."""
+        """Take the inputs from the user in proper succession."""
         while True:
             while True:
                 try:
-                    # self.num1 = float(input("Enter The First Number: "))
-                    # self.num2 = float(input("Enter The Second Number: "))
                     pprint("Enter your number")
-                    # validation check must be done
                     break
                 except ValueError:
                     pprint("Please Enter A Valid Number")
                     continue
-                # To let the user to know it is time to exit.
             pprint("Press 'q' to exit")
-        # if self.num1 == "q" or self.num2 == "q":
-        #     exit()  # Some how I need to exit it
 
     def greeting(self):
         """Greet the user with using Audio."""
         text_to_audio = "Welcome To The Calculator"
         self.gtts_object = gTTS(
             text=text_to_audio, lang="en", tld="co.in", slow=False
-        )  # ИСПРАВЛЕНО: разбита длинная строка
+        )
         tts = self.gtts_object
         fp = BytesIO()
         tts.write_to_fp(fp)
-        fp.seek(0)  # Reset the BytesIO object to the beginning
+        fp.seek(0)
         mixer.init()
         mixer.music.load(fp)
         mixer.music.play()
         while mixer.music.get_busy():
             time.Clock().tick(10)
 
-    # Here OOP is not followed.
     def user_name(self):
-        """Get the name of the user and have an option
-        to greet him/her
-        """
+        """Get the name of the user and have an option to greet him/her."""
         self.name = input("Please enter your good name: ")
-        # Making validation checks here
-        text_to_audio = f"{self.name}"  # ИСПРАВЛЕНО: исправлена f-строка
+        text_to_audio = f"{self.name}"
         self.gtts_object = gTTS(
             text=text_to_audio, lang="en", tld="co.in", slow=False
-        )  # ИСПРАВЛЕНО: разбита длинная строка
+        )
         tts = self.gtts_object
         fp = BytesIO()
         tts.write_to_fp(fp)
-        fp.seek(0)  # Reset the BytesIO object to the beginning
+        fp.seek(0)
         mixer.init()
         mixer.music.load(fp)
         mixer.music.play()
@@ -264,60 +216,27 @@ class Calculator:
             time.Clock().tick(10)
 
     def user_name_art(self):
-        """Get the name of the user and have an option
-        to show him his user name in art
-        """
-        # Default is to show = True, else False if user tries to disable it.
-
-        # Tell him to show the time and date
-        # print(art.text2art(self.name))
-        # print(date and time of now)
-        # Remove whitespaces from beginning and end
-        # Remove middle name and last name
-        # Remove special characters
-        # Remove numbers
+        """Get the name of the user and show it as art."""
         f_name = self.name.split(" ")[0]
         f_name = f_name.strip()
-        # Remove every number present in it
-        # Will have to practice not logic
         f_name = "".join([i for i in f_name if not i.isdigit()])
 
-        # perform string operations on it for the art to be displayed.
-        # Remove white spaces
-        # Remove middle name and last name
-        # Remove special characters
-        # Remove numbers
-        # Remove everything
-
     class UnitConversion:
-        """Class enriched with all the methods to convert units"""
-
-        # Do we full-stops in generating documentations?
+        """Class enriched with all the methods to convert units."""
 
         def __init__(self):
-            """Initialize the class with the required attributes."""
             self.take_inputs()
 
         def length(self):
             """Convert length units."""
-            # It should have a meter to unit and unit to meter converter
-            # Othe lengths units it should also have.
-            # Like cm to pico meter and what not
             pass
 
         def area(self):
             """Calculate area of different shapes."""
-            # This will to have multiple shapes and polygons to it
-            # to improve it's area.
-            # This will to have multiple shapes and polygons to it
-            # to improve it's area.
-            # I will try to use the best of the formula to do it
-            # like the n number of polygons to be solved.
             pass
 
         def volume(self):
             """Calculate volume of different shapes."""
-            # Different shapes and polygons to it to improve it's volume.
             pass
 
         def mass(self):
@@ -402,32 +321,8 @@ class Calculator:
 
     def clear_screen(self):
         """Clear the console screen."""
-        # Do I need a clear screen?
-        # os.system("cls" if os.name == "nt" else "clear")
-        # os.system("cls")
-        # os.system("clear")
         pass
 
 
 if __name__ == "__main__":
     operation_1 = Calculator()
-
-    # Operations
-    # User interaction
-    # Study them properly and try to understand them.
-    # Study them properly and try to understand them in very
-    # detailed length. Please.
-    # Add a function to continually ask for input until the user
-    # enters a valid input.
-
-
-# Let's explore colorma
-# Also user log ins, and it saves user data and preferences.
-# A feature of the least priority right now.
-
-# List of features priority should be planned.
-
-
-# Documentations are good to read and understand.
-# A one stop solution is to stop and read the document.
-# It is much better and easier to understand.
