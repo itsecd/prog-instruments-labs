543a3b105864bb88788000266c66e888a785485d
TimurMif <162575403+TimurMif@users.noreply.github.com>
2025-09-17 19:41:01 +0400

    Пустые строки и табуляция

--- lab_1/yahtzee.py
+++ lab_1/yahtzee.py
@@ -1,3 +1,5 @@
+
+
 class Yahtzee:
 
     @staticmethod
@@ -33,40 +35,37 @@ class Yahtzee:
             sum += 1
         if (d5 == 1): 
             sum += 1
-
         return sum
-    
 
     @staticmethod
     def twos(d1, d2, d3, d4, d5):
         sum = 0
         if (d1 == 2):
-             sum += 2
+            sum += 2
         if (d2 == 2):
-             sum += 2
+            sum += 2
         if (d3 == 2):
-             sum += 2
+            sum += 2
         if (d4 == 2):
-             sum += 2
+            sum += 2
         if (d5 == 2):
-             sum += 2
+            sum += 2
         return sum
     
     @staticmethod
     def threes(d1, d2, d3, d4, d5):
         s = 0
         if (d1 == 3):
-             s += 3
+            s += 3
         if (d2 == 3):
-             s += 3
+            s += 3
         if (d3 == 3):
-             s += 3
+            s += 3
         if (d4 == 3):
-             s += 3
+            s += 3
         if (d5 == 3):
-             s += 3
+            s += 3
         return s
-    
 
     def __init__(self, d1, d2, d3, d4, _5):
         self.dice = [0] * 5
@@ -82,7 +81,6 @@ class Yahtzee:
             if (self.dice[at] == 4): 
                 sum += 4
         return sum
-    
 
     def fives(self):
         s = 0
@@ -91,7 +89,6 @@ class Yahtzee:
             if (self.dice[i] == 5):
                 s = s + 5
         return s
-    
 
     def sixes(self):
         sum = 0
@@ -146,7 +143,6 @@ class Yahtzee:
             if (tallies[i] == 4):
                 return (i + 1) * 4
         return 0
-    
 
     @staticmethod
     def three_of_a_kind(d1, d2, d3, d4, d5):
@@ -160,7 +156,6 @@ class Yahtzee:
             if (t[i] == 3):
                 return (i + 1) * 3
         return 0
-    
 
     @staticmethod
     def smallStraight(d1, d2, d3, d4, d5):
@@ -177,7 +172,6 @@ class Yahtzee:
             tallies[4] == 1):
             return 15
         return 0
-    
 
     @staticmethod
     def largeStraight(d1, d2, d3, d4, d5):
@@ -194,7 +188,6 @@ class Yahtzee:
             tallies[5] == 1):
             return 20
         return 0
-    
 
     @staticmethod
     def fullHouse(d1, d2, d3, d4, d5):
@@ -216,13 +209,11 @@ class Yahtzee:
             if (tallies[i] == 2): 
                 _2 = True
                 _2_at = i + 1
-            
 
         for i in range(6):
             if (tallies[i] == 3): 
                 _3 = True
                 _3_at = i + 1
-            
 
         if (_2 and _3):
             return _2_at * 2 + _3_at * 3
