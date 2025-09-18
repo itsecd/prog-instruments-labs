48ddf53b03717d4e7bec48695dce099f64c2e9de
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-18 20:02:15 +0400

    Add suppress to makedirs

--- lab_4/src/database/db.py
+++ lab_4/src/database/db.py
@@ -1,3 +1,4 @@
+from contextlib import suppress
 import os
 import sqlite3
 
@@ -5,16 +6,19 @@ from src.config.paths import paths
 
 
 def get_connection() -> sqlite3.Connection:
-    conn = sqlite3.connect(paths.db_dir)
+    conn = sqlite3.connect(paths.db_file)
     conn.row_factory = sqlite3.Row
     return conn
 
 
 def init_db():
     if not os.path.exists(paths.db_file):
-        os.makedirs(os.path.dirname(paths.db_file))
+        with suppress(FileExistsError):
+            os.makedirs(os.path.dirname(paths.db_file))
+
         conn = get_connection()
         with open(paths.schema_file, "r") as file:
             conn.executescript(file.read())
+
         conn.commit()
         conn.close()
