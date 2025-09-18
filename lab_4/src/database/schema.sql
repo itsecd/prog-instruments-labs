8569029162f564842b4f42bae2c6ad2e88ba40d7
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-18 19:16:43 +0400

    Add sql schema

---
+++ lab_4/src/database/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    tg_id INTEGER PRIMARY KEY,
+    tasks_reversed INTEGER NOT NULL CHECK( tasks_reversed IN (0, 1) ) DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS tasks
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    task TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (tg_id)
+);
