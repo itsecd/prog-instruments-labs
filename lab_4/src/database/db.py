5b89a8b3c329a34bc453b710b339cc67df510f24
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-18 19:55:54 +0400

    Add db integration

---
+++ lab_4/src/database/db.py
@@ -0,0 +1,20 @@
+import os
+import sqlite3
+
+from src.config.paths import paths
+
+
+def get_connection() -> sqlite3.Connection:
+    conn = sqlite3.connect(paths.db_dir)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def init_db():
+    if not os.path.exists(paths.db_file):
+        os.makedirs(os.path.dirname(paths.db_file))
+        conn = get_connection()
+        with open(paths.schema_file, "r") as file:
+            conn.executescript(file.read())
+        conn.commit()
+        conn.close()
