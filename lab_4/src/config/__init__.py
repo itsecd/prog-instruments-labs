62d9ad825ff9dd9e248143c9ffe52b5142ceccc6
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-19 16:02:20 +0400

    Add messages

--- lab_4/src/config/__init__.py
+++ lab_4/src/config/__init__.py
@@ -1,2 +1,3 @@
 from .config_reader import config
 from .paths import paths
+from .messages import MESSAGES
---
+++ lab_4/src/config/messages.py
@@ -0,0 +1,14 @@
+MESSAGES = {
+    "message.enter_task": "Enter task:",
+    "message.your_tasks": "Your tasks:",
+    
+    "format.tasks": "{item[0]}. {item[1]}",
+    
+    "button.add": "Add",
+    "button.delete": "Delete",
+    "button.edit": "Edit",
+    "button.edit_or_delete": "Edit / Delete",
+    "button.back": "Back",
+
+    "error.something_wrong": "Something went wrong :(",
+}
