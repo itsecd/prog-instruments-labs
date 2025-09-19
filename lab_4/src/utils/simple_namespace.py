76f8be55a741c761fd19ea2eb41f545405ce71f3
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-19 16:08:43 +0400

    Add generating simple namespace from dict

---
+++ lab_4/src/utils/simple_namespace.py
@@ -0,0 +1,12 @@
+from types import SimpleNamespace
+
+
+def dict_to_sn(d: dict):
+    sn = SimpleNamespace()
+    for key, value in d.items():
+        if isinstance(value, dict):
+            setattr(sn, key, dict_to_sn(value))
+        else:
+            setattr(sn, key, dict_to_sn(value))
+    
+    return sn
