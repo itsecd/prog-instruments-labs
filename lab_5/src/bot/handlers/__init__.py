263dae47f9771b4afb039c8173668f9c0be91dda
sh1kaff <63544208+sh1kaff@users.noreply.github.com>
2025-09-21 15:12:46 +0400

    Add a honeypot and logging into it

--- lab_5/src/bot/handlers/__init__.py
+++ lab_5/src/bot/handlers/__init__.py
@@ -1,5 +1,8 @@
 from .main_menu import router as main_menu_router
+from .honeypot import router as honeypot_router
+
 
 ROUTERS = (
     main_menu_router,
+    honeypot_router,
 )
---
+++ lab_5/src/bot/handlers/honeypot.py
@@ -0,0 +1,24 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+router = Router()
+
+
+async def honeypot_base_handler(message: Message,):
+    logger.warning(
+        "Strange actions from user with id = %d, message = \"%s\"",
+        message.from_user.id,
+        message.text,
+    )
+
+
+@router.message(Command("test", "admin", "debug", "hack", "1337"))
+async def honeypot_handler(message: Message):
+    await honeypot_base_handler(message)
