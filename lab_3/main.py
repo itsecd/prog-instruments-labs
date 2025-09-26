01cc44aac3758dcadfaeea831df063250aa095d9
APMorozov <morozov-sasha200@yandex.ru>
2025-09-27 00:00:07 +0400

    Поменял подход к валидации каждого столбца, сделал универсальную функцию для этого, вынес все паттерны в отдельный файл.Добавил функцию для сбора номеров строк с ошибками

--- lab_3/main.py
+++ lab_3/main.py
@@ -2,19 +2,16 @@ from main_modules.checksum import serialize_result
 from main_modules.file_work import read_json, read_csv
 from main_modules.regexp import (validata_phone_number, validate_http_status, validate_inn, validate_identifier,
                                  validate_ipv4, validate_latitude, validate_blood_type, validate_isbn, validate_uuid,
-                                 validate_date)
+                                 validate_date, validate_by_pattern, get_rows_with_mistakes)
 
 if __name__ == "__main__":
     settings = read_json("settings.json")
-    serialize_result(14, "2006", settings["path_to_result"])
+    patterns = read_json(settings["path_to_patterns"])
+    keys = list(patterns.keys())
     data = read_csv(settings["path_to_data"])
-    idx_of_bad_values = []
-    print(data)
-    for index, value in data["date"].items():
-        if not validate_date(value):
-            print(f"Index:{index}    Number:{value}")
-            idx_of_bad_values.append(index)
-    print(len(idx_of_bad_values))
+    rows_with_mistakes = get_rows_with_mistakes(data, keys)
+    print(rows_with_mistakes)
+    print(len(rows_with_mistakes))
 
 
 
--- lab_3/main_modules/regexp.py
+++ lab_3/main_modules/regexp.py
@@ -1,3 +1,4 @@
+from pandas import DataFrame
 import re
 
 
@@ -74,3 +75,22 @@ def validate_date(date: str) -> bool:
     if re.fullmatch(pattern, date):
         return True
     return False
+
+
+def validate_by_pattern(data: str, pattern: str) -> bool:
+    sub_pattern = rf'{pattern}'
+    if re.fullmatch(sub_pattern, data):
+        return True
+    return False
+
+
+def get_rows_with_mistakes(data_frame: DataFrame, patterns) -> list:
+    rows_with_mistakes = []
+    patter_index = 0
+    for column_name in data_frame:
+        for row_index, value in data_frame[column_name].items():
+            if not validate_by_pattern(value, patterns[patter_index]):
+                if row_index not in rows_with_mistakes:
+                    rows_with_mistakes.append(row_index)
+        patter_index += 1
+    return rows_with_mistakes
