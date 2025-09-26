a727f1c5eb08f6b0c92f8bf20fbffd6c3ccd228a
APMorozov <morozov-sasha200@yandex.ru>
2025-09-27 00:16:40 +0400

    Вынес вариант в файл настройками,вычислил контрольную сумму, сериализовал ее в result.json

--- lab_3/main.py
+++ lab_3/main.py
@@ -1,15 +1,15 @@
-from main_modules.checksum import serialize_result
+from main_modules.checksum import serialize_result, calculate_checksum
 from main_modules.file_work import read_json, read_csv
 from main_modules.regexp import get_rows_with_mistakes
 
 if __name__ == "__main__":
     settings = read_json("settings.json")
-    patterns = read_json(settings["path_to_patterns"])
-    keys = list(patterns.keys())
+    patterns_dict = read_json(settings["path_to_patterns"])
+    patterns_list = list(patterns_dict.keys())
     data = read_csv(settings["path_to_data"])
-    rows_with_mistakes = get_rows_with_mistakes(data, keys)
-    print(rows_with_mistakes)
-    print(len(rows_with_mistakes))
+    rows_with_mistakes = get_rows_with_mistakes(data, patterns_list)
+    checksum = calculate_checksum(rows_with_mistakes)
+    serialize_result(settings["variant"], checksum, settings["path_to_result"])
 
 
 
