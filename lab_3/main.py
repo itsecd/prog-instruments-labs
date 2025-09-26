1b56b286e54892508f779837bbf6f942c59e6f31
APMorozov <morozov-sasha200@yandex.ru>
2025-09-27 00:02:22 +0400

    Удалил не нужные после переработки функции

--- lab_3/main.py
+++ lab_3/main.py
@@ -1,8 +1,6 @@
 from main_modules.checksum import serialize_result
 from main_modules.file_work import read_json, read_csv
-from main_modules.regexp import (validata_phone_number, validate_http_status, validate_inn, validate_identifier,
-                                 validate_ipv4, validate_latitude, validate_blood_type, validate_isbn, validate_uuid,
-                                 validate_date, validate_by_pattern, get_rows_with_mistakes)
+from main_modules.regexp import get_rows_with_mistakes
 
 if __name__ == "__main__":
     settings = read_json("settings.json")
