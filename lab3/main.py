9d9ed8a48f34f6a5030f9b5b5b4474497301572c
ct <chindina.63@gmail.com>
2025-09-28 17:13:34 +0400

    add json file with settings

--- lab3/main.py
+++ lab3/main.py
@@ -1,12 +1,22 @@
+import argparse
+
 from filehandler.filehandler import read_data, save_data, read_csv
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("settings_file", type=str, help="directory to settings file")
+    return parser.parse_args()
 
 
 def main():
     try:
-        table = read_csv("data/30.csv")
+        settings = read_data(get_args().settings_file)
+        table = read_csv(settings["csv_file"])
         print(table)
     except Exception as e:
         print(e)
+
 
 if __name__ == "__main__":
     main()
