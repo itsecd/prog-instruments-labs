ab6220964e5bea9072d0090674d72e0954e23011
ct <chindina.63@gmail.com>
2025-09-29 19:53:32 +0400

    some changes

--- lab_3/checksum.py
+++ lab_3/checksum.py
@@ -2,6 +2,8 @@ import json
 import hashlib
 from typing import List
 
+from filehandler.filehandler import save_data
+
 """
 В этом модуле обитают функции, необходимые для автоматизированной проверки результатов ваших трудов.
 """
@@ -17,7 +19,7 @@ def calculate_checksum(row_numbers: List[int]) -> str:
 
     Надеюсь, я расписал это максимально подробно.
     Хотя что-то мне подсказывает, что обязательно найдется человек, у которого с этим возникнут проблемы.
-    Которому я отвечу, что все написано в докстринге ¯\_(ツ)_/¯
+    Которому я отвечу, что все написано в докстринге
 
     :param row_numbers: список целочисленных номеров строк csv-файла, на которых были найдены ошибки валидации
     :return: md5 хеш для проверки через github action
@@ -26,7 +28,7 @@ def calculate_checksum(row_numbers: List[int]) -> str:
     return hashlib.md5(json.dumps(row_numbers).encode('utf-8')).hexdigest()
 
 
-def serialize_result(variant: int, checksum: str) -> None:
+def serialize_result(variant: int, checksum: str, directory: str) -> None:
     """
     Метод для сериализации результатов лабораторной пишите сами.
     Вам нужно заполнить данными - номером варианта и контрольной суммой - файл, лежащий в папке с лабораторной.
@@ -37,8 +39,15 @@ def serialize_result(variant: int, checksum: str) -> None:
 
     :param variant: номер вашего варианта
     :param checksum: контрольная сумма, вычисленная через calculate_checksum()
+    :param directory: path to file
     """
-    pass
+    save_data(
+        directory,
+        {
+        "variant": variant,
+        "checksum": checksum
+        }
+    )
 
 
 if __name__ == "__main__":
