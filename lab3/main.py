2b8af97ee492c53f8e47f57ddb84494217353d87
ct <chindina.63@gmail.com>
2025-09-29 14:36:33 +0400

    find indexes of invalid rows and save checksum to result.json

--- lab3/main.py
+++ lab3/main.py
@@ -1,7 +1,8 @@
 import argparse
 
-from filehandler.filehandler import read_data, read_csv
-from regular_expression.regular_expression import check_validate
+from checksum import calculate_checksum, serialize_result
+from filehandler.filehandler import read_csv, read_data
+from regular_expression.regular_expression import find_invalid_rows
 
 
 def get_args():
@@ -14,26 +15,10 @@ def main():
     try:
         settings = read_data(get_args().settings_file)
         regexp = read_data(settings["regexp"])
-        # table = read_csv(settings["csv_file"])
-        # print(table)
-        print(f"Phone number: {check_validate("+7-(969)-765-17-05", regexp["telephone"])}")
-        print(f"HTTP 1st var: {check_validate("200 OK", regexp["http_status_message"])}")
-        print(f"HTTP 2nd var: {check_validate("226 IM Used", regexp["http_status_message"])}")
-        print(f"INN 12 symbols: {check_validate("733499833600", regexp["inn"])}")
-        print(f"INN more than 12 symbols: {check_validate("7334998336009999", regexp["inn"])}")
-        print(f"Identifier: {check_validate("62-71/26", regexp["identifier"])}")
-        print(f"Identifier: {check_validate("62 71/26", regexp["identifier"])}")
-        print(f"IP_v4: {check_validate("19.121.223.58", regexp["ip_v4"])}")
-        print(f"IP_v4: {check_validate("19.121223.58", regexp["ip_v4"])}")
-        print(f"Latitude: {check_validate("-8.287791", regexp["latitude"])}")
-        print(f"Latitude: {check_validate("-8.287791 сш", regexp["latitude"])}")
-        print(f"Blood type: {check_validate("AB+", regexp["blood_type"])}")
-        print(f"Blood type: {check_validate("BA+", regexp["blood_type"])}")
-        print(f"Blood type: {check_validate("A-", regexp["blood_type"])}")
-        print(f"Blood type: {check_validate("O+", regexp["blood_type"])}")
-        print(f"uuid: {check_validate("3a7fb1ca-bdc6-4314-ad9a-6370f7a9657b", regexp["uuid"])}")
-        print(f"Date: {check_validate("2024-06-02", regexp["date"])}")
-        print(f"Date: {check_validate("2024-33-02", regexp["date"])}")
+        table = read_csv(settings["csv_file"])
+        indexes = find_invalid_rows(table, regexp)
+        checksum = calculate_checksum(list(indexes))
+        serialize_result(settings["variant"],checksum, settings["result"])
     except Exception as e:
         print(e)
 
