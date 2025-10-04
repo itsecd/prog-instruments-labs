769577e0f6f6dcb13c8362a5c61106a6621832b3
APMorozov <morozov-sasha200@yandex.ru>
2025-10-04 21:45:41 +0400

    Добавли метод проверки проверки победителя

--- lab4/blackjack.py
+++ lab4/blackjack.py
@@ -36,6 +36,30 @@ class BlackJack2:
         while self.dealer.should_hit() < self.DEALER_OPTIMAL_SUM:
             self.dealer.add_card(self.deck.deal_one())
 
+    def check_winner(self, bet: int):
+        player_sum = self.player.hand_value()
+        dealer_sum = self.dealer.hand_value()
+
+        if player_sum > 21:
+            return "dealer", 0
+
+        if dealer_sum > 21:
+            self.player.win(bet * 2)
+            return "player", bet * 2
+
+        if player_sum == 21:
+            self.player.win(bet * 3)
+            return "blackjack", bet * 3
+
+        if player_sum > dealer_sum:
+            self.player.win(bet * 2)
+            return "player", bet * 2
+        elif player_sum < dealer_sum:
+            return "dealer", 0
+        else:
+            self.player.win(bet)
+            return "tie", bet
+
 
 
 
