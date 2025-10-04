a17461a62c5b1a1001614cd42fcb2602672d91a0
APMorozov <morozov-sasha200@yandex.ru>
2025-10-04 21:17:20 +0400

    Написал функционал для класса игрока

---
+++ lab4/Player.py
@@ -0,0 +1,35 @@
+from Deck import Deck, Card
+
+class Player:
+    def __init__(self, chips):
+        self.chips = chips
+        self.hand = []
+
+    def reset_hand(self):
+        self.hand = []
+
+    def add_card(self, card: Card):
+        self.hand.append(card)
+
+    def hand_value(self):
+        total_sum = sum(card.value() for card in self.hand)
+        aces_count = sum(1 for card in self.hand if card.rank == "Ace")
+        while total_sum > 21 and aces_count:
+            total_sum -= 10
+            aces_count -= 1
+        return total_sum
+
+    def make_bet(self, bet: int):
+        if bet < 0:
+            raise ValueError("Bet must be > 0")
+        if bet > self.chips:
+            raise ValueError("Not enough chips")
+        self.chips -= bet
+        return  bet
+
+    def win(self, winnings: int):
+        self.chips += winnings
+
+    def __str__(self):
+        cards = ", ".join(str(card) for card in self.hand)
+        return f"Player`s hand: {cards}"
