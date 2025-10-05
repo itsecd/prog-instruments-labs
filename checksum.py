e5af5655d42c7af5e339cc0ed2e8788fee266172
me1ax <melirina12345@gmail.com>
2025-10-05 17:20:39 +0200

    Add files via upload

---
+++ checksum.py
@@ -0,0 +1,24 @@
+import json
+import hashlib
+from typing import List
+from file_handler import save_json
+
+"""
+В этом модуле обитают функции, необходимые для автоматизированной проверки результатов ваших трудов.
+"""
+
+
+def calculate_checksum(row_numbers: List[int]) -> str:
+    """
+    Вычисляет md5 хеш от списка целочисленных значений.
+
+    :param row_numbers: список целочисленных номеров строк csv-файла, на которых были найдены ошибки валидации
+    :return: md5 хеш для проверки через github action
+    """
+    row_numbers.sort()
+    return hashlib.md5(json.dumps(row_numbers).encode('utf-8')).hexdigest()
+
+
+def serialize_result(variant, checksum, path):
+    res = {'variant':variant, 'checksum': checksum}
+    save_json(res, path)
