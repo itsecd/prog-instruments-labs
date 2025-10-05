da674df4f83aab718a0aec0eec4cb9a157abed5c
Barnaeva <161695917+Barnaeva@users.noreply.github.com>
2025-10-05 15:19:30 +0400

    a file has been added for working with json and csv

---
+++ lab_3/io_operations.py
@@ -0,0 +1,54 @@
+import json
+import pandas as pd
+
+
+def read_csv(filename: str)->pd.DataFrame:
+    """
+    Read a csv file and return a data frame.
+    :param filename: The name of the csv file.
+    :return: Data frame.
+    """
+    try:
+        with open('3.csv', mode='r') as file:
+            df = pd.read_csv('3.csv', delimeter=';')
+            return df
+    except Exception as exc:
+        print(f"Error reading JSON: {exc}")
+
+
+
+def read_json(filename: str) -> dict:
+    """
+    Read a JSON file and return a dictionary.
+
+    :param filename: The name of the JSON file.
+    :return: A dictionary with the data.
+    """
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
+        return {}
+    except json.JSONDecodeError as e:
+        print(f"JSON format error in file {filename}: {e}")
+        return {}
+    except Exception as exc:
+        print(f"Error reading JSON: {exc}")
+        return {}
+
+
+def write_json(filename: str, data: dict) -> None:
+    """
+    Write data to a JSON file.
+
+    :param filename: The name of the file to write.
+    :param data: The data to write.
+    """
+    try:
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+    except PermissionError:
+        print(f"No permission to write to file {filename}.")
+    except Exception as exc:
+        print(f"Error writing JSON: {exc}")
