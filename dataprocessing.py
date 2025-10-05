2f44d3f3b6bd6f965d451e550f6b5fae7d4bb8b7
Deemeed <dmitrijcencov82500@gmail.com>
2025-10-05 16:41:39 +0400

    decomposition & test start

---
+++ dataprocessing.py
@@ -0,0 +1,9 @@
+import csv
+
+
+def read_csv(filename: str):
+    pass
+
+
+def validity_check(data):
+    pass
---
+++ main.py
@@ -0,0 +1,12 @@
+from checksum import calculate_checksum, serialize_result
+
+
+def main():
+    try:
+        pass
+    except Exception as exc:
+        print(f'Something went wrong: {exc}')
+
+
+if __name__ == "__main__":
+    main()
