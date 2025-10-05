716835bab911927734b71021adad3dab98007981
Deemeed <dmitrijcencov82500@gmail.com>
2025-10-05 17:24:47 +0400

    serialization & csv reading

--- dataprocessing.py
+++ dataprocessing.py
@@ -1,9 +1,16 @@
-import csv
+import pandas as pd
+from pandas import DataFrame
 
 
-def read_csv(filename: str):
+def read_csv(filename: str) -> DataFrame:
+    df = pd.read_csv(filename, sep=';', encoding='utf-16', header=0)
+
+    return df
+
+
+def validity_check(data: DataFrame):
     pass
 
-
-def validity_check(data):
-    pass
+if __name__ == "__main__":
+    data = read_csv("27.csv")
+    print(data)
