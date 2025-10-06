a7b06f37e3a2b2099613d128c84fe368d5267557
Maximord <maximyarosl@mail.ru>
2025-10-06 22:45:02 +0400

    Далее пропишем функцию, выполняющую валидацию всей строки, она использует уже написанную функцию, т.е. она просто проходит по 10 полям и каждое поле прогоняет через функцию.

--- lab_3/validator.py
+++ lab_3/validator.py
@@ -12,3 +12,11 @@ def validate_field(field_name: str, value: str) -> bool:
         return True
     pattern = PATTERNS[field_name]
     return bool(re.match(pattern, value))
+
+
+def validate_row(row: dict) -> bool:
+    """Валидация всей строки"""
+    for field, value in row.items():
+        if not validate_field(field, value):
+            return False
+    return True
