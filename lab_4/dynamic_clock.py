1c2d00f62c519dd318cedeabaaf907d3683297fd
padim <padimvetrov@gmail.com>
2025-10-06 15:14:09 +0400

    В этом файле находится класс Clock, который отвечает за логику вывода времени на данный момент. Также реализовал через класс и приватные методы, в исходном коде было через функции. Методы сделал приватными для безопасности, чтобы доступ был только у данного класса.

--- lab_4/dynamic_clock.py
+++ lab_4/dynamic_clock.py
@@ -1,27 +1,37 @@
 import tkinter as tk
 from time import strftime
+from style import STYLE
+
 
 class Clock(tk.Frame):
     """
     Clock
     """
     def __init__(self, master):
-        super().__init__(master)
-        self.pack(pady=10)
-        self.create_widgets()
-        self.update_time()
+        super().__init__(master, bg=STYLE["colors"]["bg"])
+        self.pack(pady=STYLE["padding"]["medium"])
 
+        self.__clock_label = None
 
-    def create_widgets(self):
+        self.__create_widgets()
+        self.__update_time()
+
+    def __create_widgets(self):
         """
         Create widgets for users
         :return: None
         """
-        self.clock_label = tk.Label(self, font=("Courier New", 40, "bold"), bg="black", fg="white",padx=20, pady=10)
-        self.clock_label.pack(anchor="center")
+        self.__clock_label = tk.Label(
+            self,
+            font=STYLE["fonts"]["clock"],
+            bg=STYLE["colors"]["bg"],
+            fg=STYLE["colors"]["fg"],
+            padx=STYLE["padding"]["large"],
+            pady=STYLE["padding"]["medium"],
+        )
+        self.__clock_label.pack(anchor="center")
 
-
-    def get_time_of_day(self,hour):
+    def __get_time_of_day(self, hour: int) -> str:
         """
         Get current time of day
         :param hour: current time
@@ -29,29 +39,29 @@ class Clock(tk.Frame):
         """
         if 5 <= hour < 12:
             return "Morning"
-
         elif 12 <= hour < 18:
             return "Afternoon"
-
         return "Evening"
 
-    def update_time(self):
+    def __update_time(self):
         """
         Update time of day
         :return: None
         """
         current_time = strftime("%H:%M:%S")
         hour = int(strftime("%H"))
-        time_of_day = self.get_time_of_day(hour)
+        time_of_day = self.__get_time_of_day(hour)
 
-        self.clock_label.config(text=f"{current_time}\nGood {time_of_day}")
+        self.__clock_label.config(text=f"{current_time}\nGood {time_of_day}!")
 
-        color = ("lightblue"
+        bg_color = (
+            STYLE["colors"]["morning"]
             if time_of_day == "Morning"
-            else "lightyellow" if time_of_day == "Afternoon" else "lightcoral")
+            else STYLE["colors"]["afternoon"]
+            if time_of_day == "Afternoon"
+            else STYLE["colors"]["evening"]
+        )
 
-        self.master.configure(bg=color)
-        self.after(1000, self.update_time)
+        self.master.configure(bg=bg_color)
 
-
-
+        self.after(1000, self.__update_time)
