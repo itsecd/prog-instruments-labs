64249766bb7b260632579b30b082bc4f6638a4a3
mappy12 <mappydesuu@gmail.com>
2025-10-06 20:09:04 +0400

    начал вынесение правил валидации в отдельный объект

--- lab_3/src/validator.py
+++ lab_3/src/validator.py
@@ -3,6 +3,10 @@ import re
 from files_handler import get_csv_path, load_csv
 
 
+def validate_telephone(phone):
+    pattern = r"^\+7-\((\d{3})\)-(\d{3})-(\d{2})-(\d{2})$"
+
+
 def main():
     print(load_csv(get_csv_path()))
 
---
+++ lab_3/src/validator_settings.py
@@ -0,0 +1,19 @@
+VALIDATION_RULES = {
+    "telephone": {
+        "type": 'regex',
+        "pattern":  r'^\+7-\(\d{3}\)-\d{3}-\d{2}-\d{2}$',
+        "error": 'Номер телефона должен иметь такой формат: +7-(XXX)-XXX-XX-XX'
+    },
+
+    "http_status_message": {
+        "type": 'regex',
+        "pattern": r'^\d{3} [A-za-z]+$',
+        "error": 'HTTP статус должен иметь такой формат: "Код Описание"'
+    },
+
+    "inn": {
+        "type": 'regex',
+        "pattern": r'^\d{12}$',
+        "error": "ИНН должен содержать 12 цифр, которые не разделены никакими символами"
+    }
+}
