bc6d9c1cab3777a9ad608ab017c8981d5f60905d
ct <chindina.63@gmail.com>
2025-10-06 14:18:05 +0400

    made separated padding method

--- lab_4/hybrid_crypto_system/symmetric_crypto/symmetric_crypto.py
+++ lab_4/hybrid_crypto_system/symmetric_crypto/symmetric_crypto.py
@@ -19,16 +19,20 @@ class SymmetricCrypto:
         return os.urandom(8)
 
     @staticmethod
+    def padding(text: bytes) -> bytes:
+        padder = padding.ANSIX923(64).padder()
+        return padder.update(text) + padder.finalize()
+
+    @staticmethod
     def encrypt_data(plain_text, private_bytes, encrypted_symmetric_key):
         private_key = DeSerialization.deserialization_rsa_key(private_bytes, "private")
         symmetric_key = AsymmetricCrypto.decrypt_symmetric_key(
             encrypted_symmetric_key, private_key
         )
 
-        padder = padding.ANSIX923(64).padder()
-        padded_text = padder.update(plain_text) + padder.finalize()
-
         iv = SymmetricCrypto.get_iv()
+        padded_text = SymmetricCrypto.padding(plain_text)
+
         cipher = Cipher(TripleDES(symmetric_key), modes.CBC(iv))
         encryptor = cipher.encryptor()
         encrypted_text = encryptor.update(padded_text) + encryptor.finalize()
