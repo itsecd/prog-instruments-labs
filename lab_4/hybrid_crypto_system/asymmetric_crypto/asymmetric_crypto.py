755e920d98c17cbf4b253c709337b69013db521e
ct <chindina.63@gmail.com>
2025-10-06 13:54:13 +0400

    generate_keys method generates only asymmetric keys now

--- lab_4/hybrid_crypto_system/asymmetric_crypto/asymmetric_crypto.py
+++ lab_4/hybrid_crypto_system/asymmetric_crypto/asymmetric_crypto.py
@@ -1,33 +1,21 @@
-import os
-
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding as asymmetric_padding
-
-from hybrid_crypto_system.de_serialization.de_serialization import DeSerialization
 
 
 class AsymmetricCrypto:
     @staticmethod
-    def generate_keys(key_length: int):
+    def generate_keys(key_length: int) -> tuple:
         """
-        Key generation method
+        Asymmetric key generation method
         :param key_length: key size
-        :return: encrypted_symmetric_key, serialized_private_key, serialized_public_key
+        :return: private_key, public_key
         """
-        symmetric_key = os.urandom(key_length // 8)
 
         keys = rsa.generate_private_key(public_exponent=65537, key_size=2048)
         private_key = keys
         public_key = keys.public_key()
 
-        s_private_key = DeSerialization.serialization_rsa_key(private_key, "private")
-        s_public_key = DeSerialization.serialization_rsa_key(public_key, "public")
-
-        encrypted_symmetric_key = AsymmetricCrypto.encrypt_symmetric_key(
-            symmetric_key, public_key
-        )
-
-        return encrypted_symmetric_key, s_private_key, s_public_key
+        return private_key, public_key
 
     @staticmethod
     def encrypt_symmetric_key(symmetric_key, public_key):
