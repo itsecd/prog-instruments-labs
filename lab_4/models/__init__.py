82f0a013055b0bdf7b984f2843e31e081151acf8
ilyanikoniko <iliya05163@gmail.com>
2025-10-12 21:08:34 +0300

    Добавил файл __init__.py чтобы импорт моделей работал корректно.

---
+++ lab_4/models/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+Модуль объединяющий модели TextRNN и TextCNN для удобного импорта.
+Created on 2020/5/15 22:23
+@author: phil
+"""
+
+from .text_rnn import TextRNN
+from .text_cnn import TextCNN
+
+__all__ = ["TextRNN", "TextCNN"]
