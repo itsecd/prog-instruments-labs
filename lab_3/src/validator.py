4de15fa5b77baf3acdd6ce94ec605d9f348f4331
mappy12 <mappydesuu@gmail.com>
2025-10-13 13:53:07 +0400

    добавленые функции валидации

--- lab_3/src/validator.py
+++ lab_3/src/validator.py
@@ -1,15 +1,55 @@
 import re
+import ipaddress
+import pandas as pd
+from datetime import datetime
 
-from files_handler import get_csv_path, load_csv
-
-
-def validate_telephone(phone):
-    pattern = r"^\+7-\((\d{3})\)-(\d{3})-(\d{2})-(\d{2})$"
-
-
-def main():
-    print(load_csv(get_csv_path()))
+from validator_settings import VALIDATION_RULES
 
 
-if __name__ == "__main__":
-    main()
+def validate_regex(value, rule):
+    return bool(re.fullmatch(rule["pattern"], str(value)))
+
+
+def validate_ip(value, rule):
+    try:
+        ipaddress.IPv4Address(value)
+        return True
+    except Exception:
+        return False
+
+
+def validate_range(value, rule):
+    try:
+        val = float(value)
+        return rule["min"] <= val <= rule["max"]
+    except ValueError:
+        return False
+
+
+def validate_enum(value, rule):
+    return str(value).strip() in rule["values"]
+
+
+def validate_date(value, rule):
+    try:
+        dt = datetime.strptime(value, "%Y-%m-%d")
+        return rule["min_year"] <= dt.year <= rule["max_year"]
+    except ValueError:
+        return False
+
+
+VALIDATORS = {
+    "regex": validate_regex,
+    "ip_address": validate_ip,
+    "range": validate_range,
+    "enum": validate_enum,
+    "date_logic": validate_date,
+}
+
+
+def validate_field(field_name, value):
+    rule = VALIDATION_RULES[field_name]
+    validator = VALIDATORS[rule["type"]]
+    return validator(value, rule)
+
+
--- lab_3/src/validator_settings.py
+++ lab_3/src/validator_settings.py
@@ -1,23 +1,52 @@
 VALIDATION_RULES = {
     "telephone": {
         "type": 'regex',
-        "pattern":  r'^\+7-\(\d{3}\)-\d{3}-\d{2}-\d{2}$',
-        "error": 'Номер телефона должен иметь такой формат: +7-(XXX)-XXX-XX-XX'
+        "pattern":  r'^\+7-\(\d{3}\)-\d{3}-\d{2}-\d{2}$'
     },
 
     "http_status_message": {
         "type": 'regex',
-        "pattern": r'^\d{3} [A-za-z]+$',
-        "error": 'HTTP статус должен иметь такой формат: "Код Описание"'
+        "pattern": r'^\d{3} [A-Za-z]+$'
     },
 
     "inn": {
         "type": 'regex',
-        "pattern": r'^\d{12}$',
-        "error": "ИНН должен содержать 12 цифр, которые не разделены никакими символами"
+        "pattern": r'^\d{12}$'
     },
 
     "identifier": {
-        
+        "type": 'regex',
+        "pattern": r'^\d{2}-\d{2}/\d{2}$',
+    },
+
+    "ip_v4": {
+        "type": 'ip_address'
+    },
+
+    "latitude": {
+        "type": 'range',
+        "min": -90,
+        "max": 90
+    },
+
+    "blood_type": {
+        "type": 'enum',
+        "values": ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
+    },
+
+    "isbn": {
+        "type": 'regex',
+        "pattern": r'^(?:\d{1,5}-)?\d-\d{5}-\d{3}-\d$|^\d{3}-\d-\d{5}-\d{3}-\d$'
+    },
+
+    "uuid": {
+        "type": 'regex',
+        'pattern': r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$'
+    },
+
+    "date": {
+        "type": 'date_logic',
+        "min_year": 1900,
+        "max_year": 2025
     }
 }
