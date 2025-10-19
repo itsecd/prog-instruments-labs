97d28422d569e486fbabe4f34d9f6a6ddf1cca6d
dmitrievsn <dreyui4@gmail.com>
2025-10-19 19:25:28 +0400

    adding the first 4 regular expressions

--- lab_3/validators.py
+++ lab_3/validators.py
@@ -9,7 +9,23 @@ def get_validation_patterns() -> Dict[str, Pattern]:
     :return:Column names mapped to validation patterns
     """
     patterns = {}
-    for column in COLUMNS_TO_VALIDATE:
+    patterns[COLUMN_EMAIL] = re.compile(
+        r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    )
+    patterns[COLUMN_HTTP_STATUS] = re.compile(
+        r'^\d{3} [A-Z][A-Za-z ]+[A-Za-z]$'
+    )
+    patterns[COLUMN_IP_V4] = re.compile(
+        r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+    )
+    patterns[COLUMN_HEX_COLOR] = re.compile(
+        r'^#[0-9A-Fa-f]{6}$'
+    )
+    remaining_columns = [
+        col for col in COLUMNS_TO_VALIDATE
+        if col not in patterns
+    ]
+    for column in remaining_columns:
         patterns[column] = re.compile(r'.*')
     return patterns
 
