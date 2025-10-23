bf28497644e63c248145b49eb362568192b1ea7f
Yaroslav <yaroslav.fomenko.05@mail.ru>
2025-10-23 22:29:29 +0400

    порядок импортов

--- lab_1/lab1.py
+++ lab_1/lab1.py
@@ -1,6 +1,8 @@
+import argparse
 import os
 import json
-import argparse
+from typing import Any
+
 from cryptography.hazmat.primitives import hashes, padding
 from cryptography.hazmat.primitives.asymmetric import padding as asym_padding
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -8,7 +10,6 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
-from typing import Any
 
 
 class RSACrypto:
