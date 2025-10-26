9264f6bc9104a7fcc384225276ddb770f8e4f364
mappy12 <mappydesuu@gmail.com>
2025-10-26 16:40:36 +0400

    добавлен typehint параметра

--- lab_4/src/FileReader.py
+++ lab_4/src/FileReader.py
@@ -8,7 +8,7 @@ class FileReader:
     """Reads file data by path name"""
     data: list[Entry]
 
-    def __init__(self, path) -> None:
+    def __init__(self, path: str) -> None:
         """Contstructor: writes data to class self"""
         with open(path, encoding='windows-1251') as f:
             data = json.load(f)
