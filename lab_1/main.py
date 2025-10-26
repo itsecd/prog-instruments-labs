22880ff5231c139cd71f879b9e694f32da0ec7c7
only-hell <dnslodygin20032005@gmail.com>
2025-10-26 16:53:31 +0400

    Финальное приведение кода к стандартам PEP8

--- lab_1/main.py
+++ lab_1/main.py
@@ -2,8 +2,8 @@
 Лабораторная работа №1 по курсу "Технологии и методы программирования".
 Приведение кода к стандартам PEP8.
 
-Студент: Ладыгин Денис
 Группа: 6311
+Студент: Ладыгин Денис
 
 Программа "Text Manager" для семантического анализа текста
 и поиска ответов на вопросы пользователя.
@@ -42,12 +42,12 @@ class Example(QWidget):
         super().__init__()
         # Объявление всех атрибутов экземпляра в __init__
         self.player = None
-        self.playBtn = None
-        self.pauseBtn = None
-        self.stopBtn = None
-        self.btn = None
-        self.label = None
-        self.text_label = None
+        self.play_btn = None
+        self.pause_btn = None
+        self.stop_btn = None
+        self.next_btn = None
+        self.info_label = None
+        self.title_label = None
         self.types_of_files_form = None
 
         self.init_ui()
@@ -57,28 +57,32 @@ class Example(QWidget):
         self.setGeometry(*SCREEN_SIZE)
         self.setWindowTitle('Text manager Instruction')
         self.load_mp3('media.mp3')
-        self.playBtn = QPushButton('Воспроизвести', self)
-        self.playBtn.move(70, 180)
-        self.playBtn.clicked.connect(self.player.play)
-        self.pauseBtn = QPushButton('Пауза', self)
-        self.pauseBtn.move(210, 180)
-        self.pauseBtn.clicked.connect(self.player.pause)
-        self.stopBtn = QPushButton('Стоп', self)
-        self.stopBtn.move(70, 220)
-        self.stopBtn.clicked.connect(self.player.stop)
-        self.btn = QPushButton('Дальше', self)
-        self.btn.move(210, 220)
-        self.btn.clicked.connect(self.open_types_of_files_form)
-        self.label = QLabel(self)
-        self.label.move(40, 30)
-        self.text_label = QLabel(self)
-        self.text_label.setFont(QFont('Times New Roman', 10))
+
+        self.play_btn = QPushButton('Воспроизвести', self)
+        self.play_btn.move(70, 180)
+        self.play_btn.clicked.connect(self.player.play)
+
+        self.pause_btn = QPushButton('Пауза', self)
+        self.pause_btn.move(210, 180)
+        self.pause_btn.clicked.connect(self.player.pause)
+
+        self.stop_btn = QPushButton('Стоп', self)
+        self.stop_btn.move(70, 220)
+        self.stop_btn.clicked.connect(self.player.stop)
+
+        self.next_btn = QPushButton('Дальше', self)
+        self.next_btn.move(210, 220)
+        self.next_btn.clicked.connect(self.open_types_of_files_form)
+
+        self.title_label = QLabel(self)
+        self.title_label.setFont(QFont('Times New Roman', 10))
         welcome_text = (
-            "Вас приветствует Text manager - это программа для анализа "
+            "Вас приветствует Text manager - программа для анализа "
             "содержания текста и поиска ответа на заданный вами вопрос."
         )
-        self.text_label.setText(welcome_text)
-        self.text_label.move(10, 40)
+        self.title_label.setText(welcome_text)
+        self.title_label.setWordWrap(True)  # Автоматический перенос строк
+        self.title_label.setGeometry(10, 20, 315, 100)
 
     def open_types_of_files_form(self):
         """Открывает окно выбора типа файла."""
@@ -111,18 +115,22 @@ class TypesOfFilesForm(QWidget):
         """Инициализирует пользовательский интерфейс окна."""
         self.setGeometry(*SCREEN_SIZE)
         self.setWindowTitle('Text manager')
+
         self.btn_word_file = QPushButton(self)
         self.btn_word_file.clicked.connect(self.word_form)
-        self.btn_word_file.setIcon(QIcon('word.png'))
+        self.btn_word_file.setIcon(QIcon('word_file.jpg'))
         self.btn_word_file.move(170, 140)
         self.btn_word_file.setIconSize(QSize(100, 100))
+
         self.btn_text_file = QPushButton(self)
         self.btn_text_file.clicked.connect(self.open_text_form)
-        self.btn_text_file.setIcon(QIcon('text.png'))
+        self.btn_text_file.setIcon(QIcon('text_file.jpg'))
         self.btn_text_file.move(30, 140)
         self.btn_text_file.setIconSize(QSize(100, 100))
+
         self.name_label = QLabel(self)
-        self.name_label.setText("Выберите формат файла,")
+        self.name_label.setText("Выберите формат файла,\nкоторый хотите прикрепить.")
+        self.name_label.setAlignment(QtCore.Qt.AlignCenter)
         self.name_label.move(60, 90)
         self.name_label.setFont(QFont('Times New Roman', 10))
         self.show()
@@ -131,18 +139,16 @@ class TypesOfFilesForm(QWidget):
         """Открывает диалог выбора .docx файла и считывает текст."""
         filename, _ = QFileDialog.getOpenFileName(
             self, 'Load file', '', "Word File (*.docx)")
-        self.text_input = ''
         if filename:
             doc = docx.Document(filename)
-            for par in doc.paragraphs:
-                self.text_input += par.text
+            self.text_input = "\n".join([par.text for par in doc.paragraphs])
             self.open_question_form()
         else:
             QMessageBox.warning(self, 'Error', "Файл не выбран.")
 
     def open_question_form(self):
         """Открывает окно для ввода вопроса."""
-        self.question_form = QuestionForm(self, self.text_input)
+        self.question_form = QuestionForm(text_to_analyze=self.text_input)
         self.question_form.show()
 
     def open_text_form(self):
@@ -156,8 +162,7 @@ class TextForm(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.btn_download1 = None
-        self.label = None
+        self.btn_download = None
         self.text_label = None
         self.text_input = None
         self.question_form = None
@@ -168,20 +173,21 @@ class TextForm(QWidget):
         """Инициализирует пользовательский интерфейс окна."""
         self.setGeometry(*SCREEN_SIZE)
         self.setWindowTitle('Text manager Text file')
-        self.btn_download1 = QPushButton('Отправить', self)
-        self.btn_download1.move(100, 150)
-        self.btn_download1.clicked.connect(self.open_question_form)
-        self.label = QLabel(self)
-        self.label.move(40, 30)
+
+        self.btn_download = QPushButton('Отправить', self)
+        self.btn_download.move(100, 150)
+        self.btn_download.clicked.connect(self.open_question_form)
+
         self.text_label = QLabel(self)
         self.text_label.setText("Пожалуйста введите текст.")
         self.text_label.move(100, 90)
+
         self.text_input = QLineEdit(self)
         self.text_input.move(100, 110)
 
     def open_question_form(self):
         """Открывает окно для ввода вопроса, передавая введенный текст."""
-        self.question_form = QuestionForm(self, self.text_input.text())
+        self.question_form = QuestionForm(text_to_analyze=self.text_input.text())
         self.question_form.show()
 
 
@@ -189,13 +195,11 @@ class QuestionForm(QWidget):
     """Окно для ввода текста вопроса."""
 
     def __init__(self, parent=None, text_to_analyze=""):
-        super().__init__()
+        super().__init__(parent)
         self.text_to_analyze = text_to_analyze
-        self.btn_download2 = None
-        self.label = None
+        self.btn_send = None
         self.text_label = None
         self.question_input = None
-        self.question = ""
         self.analysis_form = None
 
         self.init_ui()
@@ -204,22 +208,23 @@ class QuestionForm(QWidget):
         """Инициализирует пользовательский интерфейс окна."""
         self.setGeometry(*SCREEN_SIZE)
         self.setWindowTitle('Question Form')
-        self.btn_download2 = QPushButton('Отправить', self)
-        self.btn_download2.move(100, 150)
-        self.btn_download2.clicked.connect(self.open_analysis_form)
-        self.label = QLabel(self)
-        self.label.move(40, 30)
+
+        self.btn_send = QPushButton('Отправить', self)
+        self.btn_send.move(100, 150)
+        self.btn_send.clicked.connect(self.open_analysis_form)
+
         self.text_label = QLabel(self)
         self.text_label.setText("Пожалуйста введите текст вопроса.")
         self.text_label.move(70, 90)
+
         self.question_input = QLineEdit(self)
         self.question_input.move(100, 110)
 
     def open_analysis_form(self):
         """Запускает форму анализа, передавая текст и вопрос."""
-        self.question = self.question_input.text()
+        question = self.question_input.text()
         self.analysis_form = AnalysisForm(
-            self, self.text_to_analyze, self.question
+            text=self.text_to_analyze, question=question
         )
         self.analysis_form.show()
 
@@ -228,7 +233,7 @@ class AnalysisForm(QMainWindow):
     """Класс, выполняющий семантический анализ текста."""
 
     def __init__(self, parent=None, text="", question=""):
-        super().__init__()
+        super().__init__(parent)
         self.text_output = None
         self.result_form = None
         self.bad_result_form = None
@@ -239,39 +244,43 @@ class AnalysisForm(QMainWindow):
         """Основной метод анализа текста и поиска ответов."""
         morph = pymorphy2.MorphAnalyzer()
         text = text.replace(',', '')
-        analysis_text, analysis_question, suggestions = [], [], []
+        suggestions = []
+
         split_regex = re.compile(r'[\.!?…]')
-        for content_type, content in enumerate([text, question]):
-            sentences = filter(
-                None, [s.strip() for s in split_regex.split(content)])
-            current_analysis = []
-            for s in sentences:
-                suggestions.append(s)
-                words_analysis = []
-                for t in s.split():
-                    res = morph.parse(t)[0]
-                    exclude_tags = ["CONJ", "NPRO", "PREP", "PRCL"]
-                    if all(tag not in res.tag for tag in exclude_tags):
-                        words_analysis.append(res.normal_form.lower())
-                current_analysis.append(words_analysis)
-            if content_type == 0:
-                analysis_text = current_analysis
-            else:
-                analysis_question = [
-                    word for sent in current_analysis for word in sent
-                ]
 
-        numbers = [
-            analysis_text.index(i) for i in analysis_text
-            for j in i if j in analysis_question
-        ]
-        numbers1 = [i for i in numbers if numbers.count(i) >= 2]
-        numbers2 = sorted(list(set(numbers1)))
-        self.text_output = [suggestions[i] for i in numbers2]
+        text_sentences = [s.strip() for s in split_regex.split(text) if s.strip()]
+        question_sentences = [s.strip() for s in split_regex.split(question) if s.strip()]
 
-        with open("text manager answer.txt", 'w', encoding='utf-8') as f:
-            for item in self.text_output:
-                f.write(item + '\n')
+        suggestions.extend(text_sentences)
+
+        exclude_tags = {"CONJ", "NPRO", "PREP", "PRCL"}
+
+        def normalize_words(sentence):
+            words = []
+            for word in sentence.split():
+                parsed_word = morph.parse(word)[0]
+                if not any(tag in parsed_word.tag for tag in exclude_tags):
+                    words.append(parsed_word.normal_form.lower())
+            return words
+
+        analysis_text = [normalize_words(s) for s in text_sentences]
+        analysis_question = [word for s in question_sentences for word in normalize_words(s)]
+
+        found_indices = []
+        for i, sentence_words in enumerate(analysis_text):
+            common_words = set(sentence_words) & set(analysis_question)
+            if len(common_words) >= 2:
+                found_indices.append(i)
+
+        self.text_output = [suggestions[i] for i in found_indices]
+
+        try:
+            with open("text_manager_answer.txt", 'w', encoding='utf-8') as f:
+                for item in self.text_output:
+                    f.write(item + '\n')
+        except IOError as e:
+            print(f"Ошибка записи в файл: {e}")
+
 
         if not self.text_output:
             self.open_bad_result_form()
@@ -295,8 +304,8 @@ class ResultForm(QMainWindow):
     def __init__(self, text_output):
         super().__init__()
         self.text_output = text_output
-        self.centralwidget = None
-        self.listWidget = None
+        self.central_widget = None
+        self.list_widget = None
 
         self.init_ui()
 
@@ -304,11 +313,11 @@ class ResultForm(QMainWindow):
         """Инициализирует пользовательский интерфейс окна."""
         self.setGeometry(300, 300, 600, 400)
         self.setWindowTitle('Text manager Good Answer')
-        self.centralwidget = QtWidgets.QWidget(self)
-        self.listWidget = QListWidget(self.centralwidget)
-        self.listWidget.setGeometry(QtCore.QRect(0, 30, 600, 500))
-        self.listWidget.addItems(self.text_output)
-        self.setCentralWidget(self.centralwidget)
+        self.central_widget = QWidget(self)
+        self.list_widget = QListWidget(self.central_widget)
+        self.list_widget.setGeometry(QtCore.QRect(0, 0, 600, 400))
+        self.list_widget.addItems(self.text_output)
+        self.setCentralWidget(self.central_widget)
 
 
 class BadResultForm(QWidget):
@@ -316,17 +325,20 @@ class BadResultForm(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.text_label1 = None
+        self.text_label = None
         self.init_ui()
 
     def init_ui(self):
         """Инициализирует пользовательский интерфейс окна."""
         self.setGeometry(*SCREEN_SIZE)
         self.setWindowTitle('Text manager Bad Answer')
-        self.text_label1 = QLabel(self)
-        self.text_label1.setText(
-            "К сожалению результатов по вашему запросу не найдено.")
-        self.text_label1.move(20, 90)
+        self.text_label = QLabel(self)
+        self.text_label.setText(
+            "К сожалению, результатов по вашему запросу не найдено."
+        )
+        self.text_label.setWordWrap(True)
+        self.text_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.text_label.setGeometry(20, 90, 295, 50)
 
 
 if __name__ == '__main__':
