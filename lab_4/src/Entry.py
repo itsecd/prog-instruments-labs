86bbf34bd575b19caf07f1fb143dd2ff120d3cf8
mappy12 <mappydesuu@gmail.com>
2025-10-26 15:47:42 +0400

    переписан класс Entry, который теперь использует декоратор dataclass

--- lab_4/src/Entry.py
+++ lab_4/src/Entry.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
 class Entry:
     """Node interface"""
     telephone: str
@@ -9,15 +12,3 @@ class Entry:
     academic_degree: str
     worldview: str
     address: str
-
-    def __init__(self, dic: dict):
-        """Node constructor"""
-        self.telephone = dic['telephone']
-        self.height = dic['height']
-        self.inn = dic['inn']
-        self.passport_number = dic['passport_number']
-        self.university = dic['university']
-        self.age = dic['age']
-        self.academic_degree = dic['academic_degree']
-        self.worldview = dic['worldview']
-        self.address = dic['address']
