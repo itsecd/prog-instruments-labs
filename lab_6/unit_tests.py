20c1a52b3ce404e169efc9a9c73ee6365761109f
ct <chindina.63@gmail.com>
2025-10-27 17:23:37 +0400

    tests for longest_sequence_test function

--- lab_6/unit_tests.py
+++ lab_6/unit_tests.py
@@ -35,3 +35,22 @@ def test_equally_consecutive_bits_all_ones():
     data = "1"*128
     result = equally_consecutive_bits(data)
     assert result >= 0.01
+
+
+def test_longest_sequence_balanced_sequence():
+    data = "1100100100001111110110101010001000100001011010001100001000110100110001001100011001100010100010111000"
+    result = longest_sequence_test(data)
+    assert result >= 0.01
+
+
+def test_longest_sequence_unbalanced_sequence():
+    data = "1"*100 + "0"*28
+    result = longest_sequence_test(data)
+    assert result >= 0.01
+
+
+def test_longest_sequence_all_ones():
+    data = "1"*128
+    result = longest_sequence_test(data)
+    assert result >= 0.01
+    
