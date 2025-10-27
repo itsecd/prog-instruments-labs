0830e5363cf96d0ee36ea03ffca2d8dd5f1b09c6
ct <chindina.63@gmail.com>
2025-10-27 17:20:35 +0400

    tests for equally_consecutive_bits function

--- lab_6/unit_tests.py
+++ lab_6/unit_tests.py
@@ -17,3 +17,21 @@ def test_frequency_bit_test_all_ones():
     data = "1"*128
     result = frequency_bit_test(data)
     assert result >= 0.01
+
+
+def test_equally_consecutive_bits_balanced_sequence():
+    data = "1100100100001111110110101010001000100001011010001100001000110100110001001100011001100010100010111000"
+    result = equally_consecutive_bits(data)
+    assert result >= 0.01
+
+
+def test_equally_consecutive_bits_unbalanced_sequence():
+    data = "1"*100 + "0"*28
+    result = equally_consecutive_bits(data)
+    assert result >= 0.01
+
+
+def test_equally_consecutive_bits_all_ones():
+    data = "1"*128
+    result = equally_consecutive_bits(data)
+    assert result >= 0.01
