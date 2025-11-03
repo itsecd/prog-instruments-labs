7bc4611c6cc7c0387713436830c45a491cd37716
Andrey Preobrazhenskiy <andr163rus@inbox.ru>
2025-11-03 17:40:14 +0400

    Split changeSnake() method into 3 shorter funcs (remove big amount of if + readability)

--- lab_4/game.py
+++ lab_4/game.py
@@ -17,3 +17,31 @@ class GameBoard:
         self.snake = [((rows // 2) + i, cols // 2) for i in range(size)]
         self.food: list[tuple[int, int]] = []
         self.direction = Direction.LEFT
+        
+    def get_next_head(self) -> tuple[int, int]:
+        r, c = self.snake[0]
+        match self.direction:
+            case Direction.UP: r -= 1
+            case Direction.DOWN: r += 1
+            case Direction.LEFT: c -= 1
+            case Direction.RIGHT: c += 1
+        return (r, c)
+
+    def collides(self, head: tuple[int, int]) -> bool:
+        return (
+            head[0] < 0 or head[1] < 0 or
+            head[0] >= self.rows or head[1] >= self.cols or
+            head in self.snake
+        )
+
+    def move_snake(self) -> bool:
+        new_head = self.get_next_head()
+        if self.collides(new_head):
+            self.finished = True
+            return False
+        self.snake.insert(0, new_head)
+        if new_head in self.food:
+            self.food.remove(new_head)
+        else:
+            self.snake.pop()
+        return True
