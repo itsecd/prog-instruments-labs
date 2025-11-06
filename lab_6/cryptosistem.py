3049b646bd6456959a6cc02208aed14881b0e28d
Deemeed <dmitrijcencov82500@gmail.com>
2025-11-06 19:13:27 +0400

    PEP8

--- lab_6/cryptosistem.py
+++ lab_6/cryptosistem.py
@@ -1,5 +1,5 @@
-from symmetric import Symmetric
 from asymmetric import Asymmetric
+from symmetric import Symmetric
 
 
 class CryptoSistem:
@@ -8,10 +8,12 @@ class CryptoSistem:
         self._iv = Symmetric.iv()
 
     def generate_hybrid_keys(self) -> tuple:
+
         """
         Runs 1st scenario: generate keys
         :return: tuple (encrypted_symmetric_key, public_key, private_key)
         """
+
         symmetric_key = Symmetric.generate_symmetric_key(self._key_len)
         private_key, public_key = Asymmetric.generate_asymmetric_keys()
         c_symmetric_key = Asymmetric.encrypt_symmetric_key(public_key, symmetric_key)
@@ -19,6 +21,7 @@ class CryptoSistem:
         return c_symmetric_key, public_key, private_key
 
     def encrypt_data(self, plain_text: bytes, private_key, c_symmetric_key: bytes) -> bytes:
+
         """
         Runs 2nd scenario: encrypt text
         :param plain_text: plain text to encrypt
@@ -26,12 +29,14 @@ class CryptoSistem:
         :param c_symmetric_key: encrypted symmetric key
         :return: encrypted text
         """
+
         symmetric_key = Asymmetric.decrypt_symmetric_key(private_key, c_symmetric_key)
         encrypted_text = Symmetric.encrypt_text(plain_text, symmetric_key, self._iv)
 
         return encrypted_text
 
     def decrypt_data(self, encrypted_text: bytes, private_key, c_symmetric_key: bytes) -> str:
+
         """
         Runs 3rd scenario: decrypt text
         :param encrypted_text: encrypted text
@@ -39,6 +44,7 @@ class CryptoSistem:
         :param c_symmetric_key: encrypted symmetric key
         :return: decrypted text as string
         """
+
         symmetric_key = Asymmetric.decrypt_symmetric_key(private_key, c_symmetric_key)
         decrypted_bytes = Symmetric.decrypt_text(encrypted_text, symmetric_key, self._iv)
         decrypted_text = decrypted_bytes.decode('utf-8')
--- lab_6/test_unit.py
+++ lab_6/test_unit.py
@@ -1,33 +1,35 @@
 import pytest
+
 from asymmetric import Asymmetric
-from symmetric import Symmetric
 from cryptosistem import CryptoSistem
+from symmetric import Symmetric
 
 
 TEST_KEY = b"test_symmetric_key"
 TEST_TEXT = b"Text for testing"
-key_len = 256 # for cryptosistem
-len_iv = 16
+KEY_LEN = 256 # for cryptosistem
+LEN_IV = 16
 
 
 def test_generate_asymmetric_keys():
 
-    '''
+    """
     Test if generated keys is not empty
     :return: None
-    '''
+    """
 
     private_key, public_key = Asymmetric.generate_asymmetric_keys()
 
     assert private_key is not None
     assert public_key is not None
 
+
 def test_encrypt_decrypt_symmetric_key():
 
-    '''
+    """
     Test if original key and encrypted/decrypted key is the same
     :return: None
-    '''
+    """
 
     private_key, public_key = Asymmetric.generate_asymmetric_keys()
 
@@ -37,12 +39,13 @@ def test_encrypt_decrypt_symmetric_key():
     assert decrypted_key == TEST_KEY
     assert encrypted_key != TEST_KEY
 
+
 def test_generate_symmetric_key():
 
-    '''
+    """
     Test generating different length keys
     :return:
-    '''
+    """
 
     key_128 = Symmetric.generate_symmetric_key(128)
     key_256 = Symmetric.generate_symmetric_key(256)
@@ -51,14 +54,15 @@ def test_generate_symmetric_key():
     assert len(key_256) == 32
     assert key_128 != key_256
 
+
 def test_encrypt_decrypt_text():
 
-    '''
+    """
     Test of text encryption and decryption
     :return: None
-    '''
+    """
 
-    key = Symmetric.generate_symmetric_key(256)
+    key = Symmetric.generate_symmetric_key(KEY_LEN)
     iv = Symmetric.iv()
 
     encrypted_text = Symmetric.encrypt_text(TEST_TEXT, key, iv)
@@ -67,34 +71,37 @@ def test_encrypt_decrypt_text():
     assert decrypted_text == TEST_TEXT
     assert encrypted_text != TEST_TEXT
 
+
 def test_cryptosistem_initialization():
 
-    '''
+    """
     Test if cryptosistem initialized/initialized correctly
     :return: None
-    '''
+    """
 
-    crypto = CryptoSistem(key_len)
+    crypto = CryptoSistem(KEY_LEN)
 
-    assert crypto._key_len == key_len
+    assert crypto._key_len == KEY_LEN
     assert crypto._iv is not None
-    assert len(crypto._iv) == len_iv
+    assert len(crypto._iv) == LEN_IV
+
 
 def test_cryptosistem_generate_hybrid_keys():
 
-    '''
+    """
     Test of key generating in cryptosistem
     :return: None
-    '''
+    """
 
-    crypto = CryptoSistem(key_len)
+    crypto = CryptoSistem(KEY_LEN)
 
     c_symmetric_key, public_key, private_key = crypto.generate_hybrid_keys()
 
     assert c_symmetric_key is not None
     assert public_key is not None
     assert private_key is not None
-    assert len(c_symmetric_key) == key_len
+    assert len(c_symmetric_key) == KEY_LEN
+
 
 @pytest.mark.parametrize("texts", [
     b"Short",
@@ -104,13 +111,13 @@ def test_cryptosistem_generate_hybrid_keys():
 ])
 def test_encrypt_decrypt_different_texts(texts):
 
-    '''
+    """
     Test encryption/decryption text of different length
     :param text_input: Texts with different lengths
     :return: None
-    '''
+    """
 
-    key = Symmetric.generate_symmetric_key(256)
+    key = Symmetric.generate_symmetric_key(KEY_LEN)
     iv = Symmetric.iv()
 
     encrypted = Symmetric.encrypt_text(texts, key, iv)
@@ -121,10 +128,10 @@ def test_encrypt_decrypt_different_texts(texts):
 
 def test_generate_hybrid_keys(monkeypatch):
 
-    '''
+    """
     Test of generate_hybrid_keys function working
     :return: None
-    '''
+    """
 
     def mock_gen_symmetric(len_key):
         return b'mock_symmetric_key'
@@ -139,7 +146,7 @@ def test_generate_hybrid_keys(monkeypatch):
     monkeypatch.setattr('asymmetric.Asymmetric.generate_asymmetric_keys', mock_gen_asymmetric)
     monkeypatch.setattr('asymmetric.Asymmetric.encrypt_symmetric_key', mock_encrypt_symmetric)
 
-    crypto = CryptoSistem(key_len)
+    crypto = CryptoSistem(KEY_LEN)
     result = crypto.generate_hybrid_keys()
 
     assert result == (b'mock_encrypted_key', 'mock_public', 'mock_private')
