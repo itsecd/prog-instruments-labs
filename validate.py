96c6f1cd0b08784f034c798bb8817ca216b410f1
nprkhn <163405428+nprkhn@users.noreply.github.com>
2025-11-07 15:28:41 +0400

    Add files via upload

---
+++ validate.py
@@ -0,0 +1,28 @@
+import re
+from typing import List
+
+def validate_row(row: dict, patterns: dict[str, str]) -> bool:
+    """
+    Check validate rows.
+
+    :row: dictionary with row's data
+    :patterns: regular expressions
+
+    :return: True or False
+    """
+    for field, pattern in patterns.items():
+        value = row.get(field, '').strip()
+        if not re.fullmatch(pattern, value):
+            return False
+    return True
+
+def invalid_validation_rows(rows: List[dict], patterns: dict[str, str]) -> List[int]:
+    """
+    Check numbers invalid rows.
+    :rows: data
+    :patterns: regular expressions
+
+    :return: number's of invalid rows 
+    """
+    numbers = [i for i, row in enumerate(rows) if not validate_row(row, patterns)]
+    return numbers
