ee7e9c710d0ef65835a7cb401244cd5009c174bd
Kirill <kirillstengauer@gmail.com>
2025-11-08 19:27:48 +0400

    create handle_special_offers

--- teller.py
+++ teller.py
@@ -25,7 +25,44 @@ class Teller:
         self.handle_offers(receipt, the_cart)
 
         return receipt
+      
     
+    def handle_special_offers(self, offer, quantity, unit_price, p)->int:
+        if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and quantity<=2:
+            return None
+        if offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT and quantity<2:
+            return None
+        if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and quantity<5:
+            return None
+
+        if offer.offer_type == SpecialOfferType.THREE_FOR_TWO:
+            x = 3
+            number_of_x = quantity//x
+            discount_amount = quantity * unit_price - ((number_of_x * 2 * unit_price) + quantity % 3 * unit_price)
+            discount = Discount(p, "3 for 2", -discount_amount)
+            
+        elif offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT:
+            x = 2
+            number_of_x = quantity//x
+            total = offer.argument * (quantity // x) + quantity % 2 * unit_price
+            discount_n = unit_price * quantity - total
+            discount = Discount(p, "2 for " + str(offer.argument), -discount_n)
+                
+        elif offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT:
+            x = 5
+            number_of_x = quantity//x
+            discount_total = unit_price * quantity - (
+                offer.argument * number_of_x + quantity % 5 * unit_price)
+            discount = Discount(p, str(x) + " for " + str(offer.argument), -discount_total)
+
+        
+        elif offer.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
+            discount = Discount(p, str(offer.argument) + "% off",
+                                -quantity * unit_price * offer.argument / 100.0)
+            
+        return discount
+    
+
     def handle_offers(self, receipt, cart):
         for p in cart.product_quantities.keys():
             quantity = cart.product_quantities[p]
@@ -33,35 +70,7 @@ class Teller:
                 offer = self.offers[p]
                 unit_price = self.catalog.unit_price(p)
                 quantity_as_int = int(quantity)
-                discount = None
-                x = 1
-                if offer.offer_type == SpecialOfferType.THREE_FOR_TWO:
-                    x = 3
-
-                elif offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT:
-                    x = 2
-                    if quantity_as_int >= 2:
-                        total = offer.argument * (quantity_as_int / x) + quantity_as_int % 2 * unit_price
-                        discount_n = unit_price * quantity - total
-                        discount = Discount(p, "2 for " + str(offer.argument), -discount_n)
-
-                if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT:
-                    x = 5
-
-                number_of_x = math.floor(quantity_as_int / x)
-                if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and quantity_as_int > 2:
-                    discount_amount = quantity * unit_price - (
-                                (number_of_x * 2 * unit_price) + quantity_as_int % 3 * unit_price)
-                    discount = Discount(p, "3 for 2", -discount_amount)
-
-                if offer.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
-                    discount = Discount(p, str(offer.argument) + "% off",
-                                        -quantity * unit_price * offer.argument / 100.0)
-
-                if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and quantity_as_int >= 5:
-                    discount_total = unit_price * quantity - (
-                                offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
-                    discount = Discount(p, str(x) + " for " + str(offer.argument), -discount_total)
-
+                
+                discount = self.handle_special_offers(offer, quantity_as_int, unit_price, p)
                 if discount:
                     receipt.add_discount(discount)
