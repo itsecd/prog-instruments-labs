39a9a88bc6db8cbffb6eede94d56ac831167ee7e
Kirill <kirillstengauer@gmail.com>
2025-11-08 18:56:50 +0400

    move handle_offers to Teller due to SRP

--- teller.py
+++ teller.py
@@ -1,4 +1,5 @@
-from model_objects import Offer
+import math
+from model_objects import Discount, Offer, SpecialOfferType
 from receipt import Receipt
 
 
@@ -21,6 +22,46 @@ class Teller:
             price = quantity * unit_price
             receipt.add_product(p, quantity, unit_price, price)
 
-        the_cart.handle_offers(receipt, self.offers, self.catalog)
+        self.handle_offers(receipt, the_cart)
 
         return receipt
+    
+    def handle_offers(self, receipt, cart):
+        for p in cart.product_quantities.keys():
+            quantity = cart.product_quantities[p]
+            if p in self.offers.keys():
+                offer = self.offers[p]
+                unit_price = self.catalog.unit_price(p)
+                quantity_as_int = int(quantity)
+                discount = None
+                x = 1
+                if offer.offer_type == SpecialOfferType.THREE_FOR_TWO:
+                    x = 3
+
+                elif offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT:
+                    x = 2
+                    if quantity_as_int >= 2:
+                        total = offer.argument * (quantity_as_int / x) + quantity_as_int % 2 * unit_price
+                        discount_n = unit_price * quantity - total
+                        discount = Discount(p, "2 for " + str(offer.argument), -discount_n)
+
+                if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT:
+                    x = 5
+
+                number_of_x = math.floor(quantity_as_int / x)
+                if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and quantity_as_int > 2:
+                    discount_amount = quantity * unit_price - (
+                                (number_of_x * 2 * unit_price) + quantity_as_int % 3 * unit_price)
+                    discount = Discount(p, "3 for 2", -discount_amount)
+
+                if offer.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
+                    discount = Discount(p, str(offer.argument) + "% off",
+                                        -quantity * unit_price * offer.argument / 100.0)
+
+                if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and quantity_as_int >= 5:
+                    discount_total = unit_price * quantity - (
+                                offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
+                    discount = Discount(p, str(x) + " for " + str(offer.argument), -discount_total)
+
+                if discount:
+                    receipt.add_discount(discount)
