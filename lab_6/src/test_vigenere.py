442a6cf9dbec8e58d6997e85f6df6298dbf26608
mappy12 <mappydesuu@gmail.com>
2025-11-14 11:09:43 +0400

    тест на проверку корректной работоспособности метода get_key_symb(), при пустом ключе

--- lab_6/src/test_vigenere.py
+++ lab_6/src/test_vigenere.py
@@ -8,3 +8,7 @@ def test_get_key_symb_basic():
     key = "кошка"
     idx = 2
     assert vmodule.get_key_symb(key, idx) == "ш"
+
+def test_get_key_symb_empty_key():
+    with pytest.raises(ValueError):
+        vmodule.get_key_symb("", 6)
