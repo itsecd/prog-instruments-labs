318464fefc64951f3cfa5580d3147783e399b21c
mappy12 <mappydesuu@gmail.com>
2025-11-14 11:11:24 +0400

    тест на проверку метода get_key_symb() с параметризацией для разных ключей и индексов, включая большие индексы

--- lab_6/src/test_vigenere.py
+++ lab_6/src/test_vigenere.py
@@ -12,3 +12,19 @@ def test_get_key_symb_basic():
 def test_get_key_symb_empty_key():
     with pytest.raises(ValueError):
         vmodule.get_key_symb("", 6)
+
+@pytest.mark.parametrize(
+    "key, index, expected",
+    [
+        ("cat", 0, "c"),
+        ("CAT", 5, "T"),
+        ("goodbye", 1, "o"),
+        ("рюкзак", 9, "з"),
+        ("ноутбук", 6, "к"),
+        ("dog", 1234567, None)
+    ]
+)
+def test_get_key_symb_param(key, index, expected):
+    if expected is None:
+        expected = key[index % len(key)]
+    assert vmodule.get_key_symb(key, index) == expected
